b99f5636ebbac3840e25e48944fa61b1bcf6c9a9
mghilardelli <marco.ghilardelli@sbb.ch>
2024-03-01 15:13:51 +0100

    feat: rename method

--- playground-backend/src/main/java/ch/sbb/playgroundbackend/controller/HelloWorldController.java
+++ playground-backend/src/main/java/ch/sbb/playgroundbackend/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping
-    String hello() {
+    String get() {
         return "Hello DAS Playground";
     }
 
