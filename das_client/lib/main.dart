872d10e466197c3ad05d34574863cd422db80c91
Thomas <thomas.bomatter@bluewin.ch>
2024-03-06 16:59:23 +0100

    Update main.dart

--- das_client/lib/main.dart
+++ das_client/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'DAS Client Home Page :)'),
+      home: const MyHomePage(title: 'DAS Client Home Page :('),
     );
   }
 }
