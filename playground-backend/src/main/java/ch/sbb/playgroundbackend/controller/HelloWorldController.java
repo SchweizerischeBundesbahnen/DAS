08d55f7e9fb25b1a3440ddb500c0e7531d3aa40c
mghilardelli <marco.ghilardelli@sbb.ch>
2024-04-22 14:34:32 +0200

    fix: jvm tool options

--- playground-backend/src/main/java/ch/sbb/playgroundbackend/controller/HelloWorldController.java
+++ playground-backend/src/main/java/ch/sbb/playgroundbackend/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping()
+    @GetMapping
     String get() {
         return "Hello DAS Test";
     }
