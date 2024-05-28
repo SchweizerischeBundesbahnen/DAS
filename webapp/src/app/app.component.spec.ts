418b8de48da3cbb262b3099c6e75b37beb63bfee
mghilardelli <marco.ghilardelli@sbb.ch>
2024-05-28 16:34:20 +0200

    fix: tests

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,10 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from "./auth.service";
+import { MqService } from "./mq.service";
+
+const mockAuth: Partial<AuthService> = {};
+const mockMq: Partial<MqService> = {};
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        {provide: AuthService, useValue: mockAuth},
+        {provide: MqService, useValue: mockMq}
+      ]
     }).compileComponents();
   });
 
