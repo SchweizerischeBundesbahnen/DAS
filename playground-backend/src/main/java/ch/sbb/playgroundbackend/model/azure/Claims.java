d6147473b82f1ccd616d11de65d3e3862651295d
u221711 <thomas.bomatter@sbb.ch>
2024-06-13 14:12:08 +0200

    chore: adjust claim names to conform to azure

--- playground-backend/src/main/java/ch/sbb/playgroundbackend/model/azure/Claims.java
+++ playground-backend/src/main/java/ch/sbb/playgroundbackend/model/azure/Claims.java
@@ -2,7 +2,7 @@ package ch.sbb.playgroundbackend.model.azure;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Claims(@JsonProperty("http://uic.org/90940/ru") String ru,
-                     @JsonProperty("http://uic.org/90940/train") String train,
-                     @JsonProperty("http://uic.org/90940/role") String role) {
+public record Claims(@JsonProperty("ru") String ru,
+                     @JsonProperty("train") String train,
+                     @JsonProperty("role") String role) {
 }
