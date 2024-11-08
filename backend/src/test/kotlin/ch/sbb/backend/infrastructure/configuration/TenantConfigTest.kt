475a4a41983b20d718f026445924bf236b1a41aa
mghilardelli <marco.ghilardelli@sbb.ch>
2024-11-08 08:52:05 +0100

    fix: tests

--- backend/src/test/kotlin/ch/sbb/backend/infrastructure/configuration/TenantConfigTest.kt
+++ backend/src/test/kotlin/ch/sbb/backend/infrastructure/configuration/TenantConfigTest.kt
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("test")
 class TenantConfigTest {
 
     @Autowired
@@ -21,7 +19,13 @@ class TenantConfigTest {
         assertEquals(tenants.size, 1)
         assertEquals(tenants[0].name, "test")
         assertEquals(tenants[0].id, "3409e798-d567-49b1-9bae-f0be66427c54")
-        assertEquals(tenants[0].issuerUri, "https://login.microsoftonline.com/3409e798-d567-49b1-9bae-f0be66427c54/v2.0")
-        assertEquals(tenants[0].jwkSetUri, "https://login.microsoftonline.com/3409e798-d567-49b1-9bae-f0be66427c54/discovery/v2.0/keys")
+        assertEquals(
+            tenants[0].issuerUri,
+            "https://login.microsoftonline.com/3409e798-d567-49b1-9bae-f0be66427c54/v2.0"
+        )
+        assertEquals(
+            tenants[0].jwkSetUri,
+            "https://login.microsoftonline.com/3409e798-d567-49b1-9bae-f0be66427c54/discovery/v2.0/keys"
+        )
     }
 }
