7d686465c856402b2ef61be454201372a439b4b7
u221711 <thomas.bomatter@sbb.ch>
2024-11-12 17:05:31 +0100

    cleanup

--- das_client/lib/mqtt/src/mqtt_client_oauth_connector.dart
+++ das_client/lib/mqtt/src/mqtt_client_oauth_connector.dart
@@ -1,16 +1,12 @@
 import 'package:das_client/auth/authentication_component.dart';
 import 'package:das_client/mqtt/src/mqtt_client_connector.dart';
-import 'package:das_client/sfera/sfera_component.dart';
 import 'package:fimber/fimber.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 
 class MqttClientOauthConnector implements MqttClientConnector {
-  final SferaAuthService _sferaAuthService;
   final Authenticator _authenticator;
 
-  MqttClientOauthConnector({required SferaAuthService sferaAuthService, required Authenticator authenticator})
-      : _sferaAuthService = sferaAuthService,
-        _authenticator = authenticator;
+  MqttClientOauthConnector({required Authenticator authenticator}) : _authenticator = authenticator;
 
   @override
   Future<bool> connect(MqttClient client, String company, String train) async {
