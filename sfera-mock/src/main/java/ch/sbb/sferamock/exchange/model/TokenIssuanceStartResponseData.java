ab17b6247503d6bcee182828ac1ca5823b3e5d17
Marco <marco.ghilardelli@sbb.ch>
2024-12-05 15:48:29 +0100

    fix: spring cloud dependencies (#448)

--- sfera-mock/src/main/java/ch/sbb/sferamock/exchange/model/TokenIssuanceStartResponseData.java
+++
@@ -1,8 +0,0 @@
-package ch.sbb.sferamock.exchange.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public record TokenIssuanceStartResponseData(@JsonProperty("@odata.type") String type, List<Action> actions) {
-}
