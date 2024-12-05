657257bb1dbbb9d695a48410d074acd316c5df5f
Marco <marco.ghilardelli@sbb.ch>
2024-12-05 16:03:51 +0100

    feat: train identifier persistence (#217) (#391)

---
+++ preload/src/main/kotlin/ch/sbb/das/preload/infrastructure/repositories/TrainIdentifiersRepository.kt
@@ -0,0 +1,9 @@
+package ch.sbb.das.preload.infrastructure.repositories
+
+import ch.sbb.das.preload.infrastructure.entities.TrainIdentifierEntity
+import ch.sbb.das.preload.infrastructure.entities.TrainIdentifierId
+import org.springframework.data.repository.ListCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TrainIdentifiersRepository : ListCrudRepository<TrainIdentifierEntity, TrainIdentifierId>
