6966b0937773c41c52bb2f9d5915ad281691bee4
mghilardelli <marco.ghilardelli@sbb.ch>
2025-01-06 15:26:22 +0100

    chore: clean test

--- backend/src/test/kotlin/ch/sbb/backend/logging/DomainLoggingServiceTest.kt
+++ backend/src/test/kotlin/ch/sbb/backend/logging/DomainLoggingServiceTest.kt
@@ -10,13 +10,7 @@ import ch.sbb.backend.logging.infrastructure.SplunkLoggingRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContext
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.oauth2.jwt.Jwt
-import java.time.Instant
 import java.time.OffsetDateTime
-import java.util.*
 
 class DomainLoggingServiceTest {
 
@@ -24,25 +18,11 @@ class DomainLoggingServiceTest {
     private lateinit var tenantRepository: TenantRepository
     private lateinit var splunkLoggingRepository: SplunkLoggingRepository
 
-    private val tid = UUID.randomUUID().toString()
-
     @BeforeEach
     fun setUp() {
         tenantRepository = mock(TenantRepository::class.java)
         splunkLoggingRepository = mock(SplunkLoggingRepository::class.java)
         sut = DomainLoggingService(splunkLoggingRepository)
-        val securityContext: SecurityContext = mock(SecurityContext::class.java)
-        val jwt = Jwt(
-            "token",
-            Instant.now(),
-            Instant.now(),
-            mapOf("header" to "value"),
-            mapOf("tid" to tid)
-        )
-        val authentication = mock(Authentication::class.java)
-        `when`(authentication.principal).thenReturn(jwt)
-        `when`(securityContext.authentication).thenReturn(authentication)
-        SecurityContextHolder.setContext(securityContext)
     }
 
     @Test
