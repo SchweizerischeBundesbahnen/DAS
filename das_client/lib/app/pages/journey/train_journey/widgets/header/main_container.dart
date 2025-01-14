68ffd4a05f8c7bf09b0a18a76b68c477cd5c9d41
u221638 <ralf.winkelmann2@sbb.ch>
2025-01-14 16:54:00 +0100

    chore: merge conflict fixed. (#82)

--- das_client/lib/app/pages/journey/train_journey/widgets/header/main_container.dart
+++ das_client/lib/app/pages/journey/train_journey/widgets/header/main_container.dart
@@ -4,11 +4,10 @@ import 'package:das_client/app/pages/journey/train_journey/widgets/header/depart
 import 'package:das_client/app/pages/journey/train_journey/widgets/header/radio_channel.dart';
 import 'package:das_client/app/widgets/assets.dart';
 import 'package:das_client/app/widgets/das_text_styles.dart';
-import 'package:das_client/app/widgets/widget_extensions.dart';
-import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
+import 'package:das_client/model/journey/journey.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:das_client/model/journey/journey.dart';
+import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
 class MainContainer extends StatelessWidget {
   const MainContainer({super.key});
