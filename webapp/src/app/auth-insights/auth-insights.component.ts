af83b224d7ad5edd3268c805ac1c14f0a855f9d4
Marco <marco.ghilardelli@sbb.ch>
2025-02-07 16:53:59 +0100

    fix: remove custom claims exchange fields (#556)

--- webapp/src/app/auth-insights/auth-insights.component.ts
+++ webapp/src/app/auth-insights/auth-insights.component.ts
@@ -1,28 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { SbbButtonModule } from "@sbb-esta/angular/button";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { map } from "rxjs";
 import { OidcSecurityService } from "angular-auth-oidc-client";
-import { SbbInputModule } from "@sbb-esta/angular/input";
 
 @Component({
   selector: 'app-auth-insights',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    SbbButtonModule,
-    SbbInputModule,
   ],
   templateUrl: './auth-insights.component.html',
   styleUrl: './auth-insights.component.scss'
 })
 export class AuthInsightsComponent {
-
-  ruControl = new FormControl('1088', {nonNullable: true});
-  trainControl = new FormControl(new Date().toISOString().split('T')[0] + '_9232', {nonNullable: true});
-  roleControl = new FormControl('read-only', {nonNullable: true});
 
   get claims() {
     return this.oidcSecurityService.getUserData();
