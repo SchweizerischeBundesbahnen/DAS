c2b054daeb05623f9327685a075cde6ea54ee302
u221711 <thomas.bomatter@sbb.ch>
2025-02-18 10:43:22 +0100

    new test try

--- das_client/integration_test/test/automatic_advancement_test.dart
+++ das_client/integration_test/test/automatic_advancement_test.dart
@@ -1,5 +1,4 @@
 import 'package:das_client/app/pages/journey/train_journey/widgets/table/cells/route_chevron.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../app_test.dart';
@@ -26,6 +25,8 @@ void main() {
       }
     });
 
+    /*
+
     testWidgets('check scrolling after idle time', (tester) async {
       // Load app widget.
       await prepareAndStartApp(tester);
@@ -51,8 +52,6 @@ void main() {
       // Check if the last row is visible
       expect(findDASTableRowByText('Olten'), findsAny);
     });
-
-    /*
 
     testWidgets('check scrolling to position if automatic scrolling gets enabled', (tester) async {
       // Load app widget.
