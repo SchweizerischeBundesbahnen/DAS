1cb4ac8bb50b7de15f45be2d7674ffd9cce99d63
Raphael Schreiber (IT-PTR-CEN1-BDE22-Extern) <raphael_mathieu.schreiber@sbb.ch>
2025-03-21 15:13:48 +0100

    chore: change the color of "date is not today" warning at the train journey selection

--- das_client/lib/app/widgets/header.dart
+++ das_client/lib/app/widgets/header.dart
@@ -1,5 +1,4 @@
 import 'package:das_client/app/widgets/das_text_styles.dart';
-import 'package:das_client/theme/theme_util.dart';
 import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 import 'package:flutter/material.dart';
 
@@ -62,12 +61,12 @@ class Header extends StatelessWidget {
           Icon(
             SBBIcons.circle_information_small,
             size: 20.0,
-            color: ThemeUtil.getFontColor(context),
+            color: SBBColors.black,
           ),
           Text(
             information!,
             style: DASTextStyles.smallLight.copyWith(
-              color: ThemeUtil.getFontColor(context),
+              color: SBBColors.black,
             ),
           ),
         ],
