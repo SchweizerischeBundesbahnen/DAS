c6b38fa741a2f6e71273510229fdedf46446b0e2
u221711 <thomas.bomatter@sbb.ch>
2025-03-24 11:55:26 +0100

    chore: fix overview next stop color

--- das_client/lib/app/pages/journey/train_journey/widgets/reduced_overview/rows/reduced_service_point_row.dart
+++ das_client/lib/app/pages/journey/train_journey/widgets/reduced_overview/rows/reduced_service_point_row.dart
@@ -2,10 +2,11 @@ import 'package:das_client/app/pages/journey/train_journey/widgets/table/cells/r
 import 'package:das_client/app/pages/journey/train_journey/widgets/table/service_point_row.dart';
 import 'package:das_client/app/widgets/table/das_table_cell.dart';
 import 'package:flutter/material.dart';
+import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
 class ReducedServicePointRow extends ServicePointRow {
   ReducedServicePointRow({required super.metadata, required super.data, required this.context, super.config})
-      : super(context: context);
+      : super(context: context, rowColor: SBBColors.white);
 
   final BuildContext context;
 
