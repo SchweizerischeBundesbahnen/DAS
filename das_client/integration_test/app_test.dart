1aa8958bf0e933ce737f4d4e1819777d13544d50
u221638 <ralf.winkelmann2@sbb.ch>
2025-04-16 14:14:23 +0200

    chore: update Flutter to 3.29.3 and dependencies

--- das_client/integration_test/app_test.dart
+++ das_client/integration_test/app_test.dart
@@ -1,8 +1,8 @@
+import 'package:das_client/app/i18n/gen/app_localizations_de.dart';
 import 'package:das_client/app/i18n/i18n.dart';
 import 'package:das_client/flavor.dart';
 import 'package:das_client/main.dart';
 import 'package:fimber/fimber.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations_de.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
