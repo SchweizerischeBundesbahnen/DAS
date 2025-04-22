72999419909bb6483039fb55b20971e8ed81e722
u221638 <ralf.winkelmann2@sbb.ch>
2025-04-22 15:06:23 +0200

    feat: add contact details to view model

--- das_client/lib/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_modal_sheet_view_model.dart
+++ das_client/lib/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_modal_sheet_view_model.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:das_client/app/pages/journey/train_journey/automatic_advancement_controller.dart';
 import 'package:das_client/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_modal_sheet_tab.dart';
 import 'package:das_client/app/widgets/modal_sheet/das_modal_sheet.dart';
+import 'package:das_client/model/journey/contact_list.dart';
+import 'package:das_client/model/journey/metadata.dart';
 import 'package:das_client/model/journey/service_point.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -14,6 +16,8 @@ class DetailModalSheetViewModel {
   final AutomaticAdvancementController automaticAdvancementController;
   late DASModalSheetController controller;
 
+  final _rxRadioContactList = BehaviorSubject<RadioContactList?>();
+  final _rxMetadata = BehaviorSubject<Metadata>();
   final _rxServicePoint = BehaviorSubject<ServicePoint>();
   final _rxIsModalSheetOpen = BehaviorSubject.seeded(false);
   final _rxSelectedTab = BehaviorSubject.seeded(DetailModalSheetTab.values.first);
@@ -25,7 +29,23 @@ class DetailModalSheetViewModel {
 
   Stream<ServicePoint> get servicePoint => _rxServicePoint.distinct();
 
+  Stream<RadioContactList?> get radioContacts => _rxRadioContactList.distinct();
+
   void _init() {
+    _initController();
+    _initRadioContacts();
+  }
+
+  void _initRadioContacts() {
+    final subscription = Rx.combineLatest2(
+      _rxServicePoint.stream,
+      _rxMetadata.stream,
+      (servicePoint, metadata) => metadata.radioContactLists.lastLowerThan(servicePoint.order),
+    ).listen(_rxRadioContactList.add, onError: _rxRadioContactList.addError);
+    _subscriptions.add(subscription);
+  }
+
+  void _initController() {
     controller = DASModalSheetController(
       isAutomaticCloseActive: automaticAdvancementController.isActive,
       onClose: () => _rxIsModalSheetOpen.add(false),
@@ -41,6 +61,8 @@ class DetailModalSheetViewModel {
         .listen((value) => controller.setAutomaticClose(isActivated: value));
     _subscriptions.add(subscription);
   }
+
+  void updateMetadata(Metadata metadata) => _rxMetadata.add(metadata);
 
   void open({DetailModalSheetTab? tab, ServicePoint? servicePoint}) {
     if (tab != null) {
@@ -63,6 +85,7 @@ class DetailModalSheetViewModel {
     for (final subscription in _subscriptions) {
       subscription.cancel();
     }
+    _rxMetadata.close();
     _rxSelectedTab.close();
     _rxIsModalSheetOpen.close();
     _rxServicePoint.close();
--- das_client/lib/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_tab_radio_channels.dart
+++ das_client/lib/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_tab_radio_channels.dart
@@ -1,7 +1,9 @@
-import 'package:das_client/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_modal_sheet_tab.dart';
+import 'package:das_client/app/pages/journey/train_journey/widgets/detail_modal_sheet/detail_modal_sheet_view_model.dart';
+import 'package:das_client/model/journey/contact_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
-// TODO: add implementation for tab views
 class DetailTabRadioChannels extends StatelessWidget {
   static const radioChannelsTabKey = Key('radioChannelsTabKey');
 
@@ -9,11 +11,28 @@ class DetailTabRadioChannels extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    final viewModel = context.read<DetailModalSheetViewModel>();
+    return StreamBuilder(
       key: radioChannelsTabKey,
-      child: Text(
-        DetailModalSheetTab.radioChannels.localized(context),
-      ),
+      stream: viewModel.radioContacts,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.data == null) {
+          // TODO: Add not found text
+          return Center(child: Text('Not found'));
+        }
+        final contactList = snapshot.data!;
+        return _contactList(contactList);
+      },
+    );
+  }
+
+  ListView _contactList(RadioContactList contactList) {
+    return ListView.builder(
+      itemCount: contactList.mainContacts.length,
+      itemBuilder: (context, index) {
+        final contact = contactList.mainContacts.elementAt(index);
+        return SBBListItem(title: contact.contactRole ?? '', subtitle: contact.contactIdentifier, onPressed: null);
+      },
     );
   }
 }
