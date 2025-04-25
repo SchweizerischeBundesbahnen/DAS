c07c6a6be473e272aeada97c22ba995156eb0038
mghilardelli <marco.ghilardelli@sbb.ch>
2025-04-25 08:58:41 +0200

    alter if exists

--- das_backend/src/main/resources/db/migration/V1.1__company_and_ru_feature_tables.sql
+++ das_backend/src/main/resources/db/migration/V1.1__company_and_ru_feature_tables.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS company
     short_name_zis TEXT
 );
 
-ALTER TABLE company
+ALTER TABLE IF EXISTS company
     ADD CONSTRAINT company_id_pk PRIMARY KEY (id);
-ALTER TABLE company
+ALTER TABLE IF EXISTS company
     ADD CONSTRAINT company_code_rics_unique UNIQUE (code_rics);
-ALTER TABLE company
+ALTER TABLE IF EXISTS company
     ADD CONSTRAINT company_code_rics_short_name_zis_unique UNIQUE (code_rics, short_name_zis);
 
 CREATE SEQUENCE company_id_seq START WITH 1 INCREMENT BY 1;
@@ -24,11 +24,11 @@ CREATE TABLE IF NOT EXISTS ru_feature
     last_modified_by TEXT      NOT NULL
 );
 
-ALTER TABLE ru_feature
+ALTER TABLE IF EXISTS ru_feature
     ADD CONSTRAINT ru_feature_id_pk PRIMARY KEY (id);
-ALTER TABLE ru_feature
+ALTER TABLE IF EXISTS ru_feature
     ADD CONSTRAINT ru_feature_company_id_fk FOREIGN KEY (company_id) REFERENCES company (id);
-ALTER TABLE ru_feature
+ALTER TABLE IF EXISTS ru_feature
     ADD CONSTRAINT ru_feature_company_id_name_unique UNIQUE (company_id, name);
 
 CREATE SEQUENCE ru_feature_id_seq START WITH 1 INCREMENT BY 1;
