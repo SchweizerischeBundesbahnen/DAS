d4326e28d5382c8930dfa48c830b848856a41230
u221711 <thomas.bomatter@sbb.ch>
2025-04-30 12:49:01 +0200

    split ios tests into multiple files, use newest xcode

---
+++ das_client/integration_test/app_test_ios_1.dart
@@ -0,0 +1,15 @@
+import 'package:das_client/app/i18n/gen/app_localizations_de.dart';
+import 'package:das_client/app/i18n/i18n.dart';
+import 'package:fimber/fimber.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'test/train_journey_table_test.dart' as train_journey_table_tests;
+
+AppLocalizations l10n = AppLocalizationsDe();
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  Fimber.plantTree(DebugTree());
+
+  train_journey_table_tests.main();
+}
