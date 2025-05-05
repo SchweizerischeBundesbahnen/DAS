00c97088c4c74150a0377ebae840d7bafc7eb3f2
u221638 <ralf.winkelmann2@sbb.ch>
2025-05-05 12:09:58 +0200

    chore: fixed wrong imports from merge

--- das_client/app/integration_test/app_test_ios_3.dart
+++ das_client/app/integration_test/app_test_ios_3.dart
@@ -1,5 +1,5 @@
-import 'package:das_client/app/i18n/gen/app_localizations_de.dart';
-import 'package:das_client/app/i18n/i18n.dart';
+import 'package:app/app/i18n/gen/app_localizations_de.dart';
+import 'package:app/app/i18n/i18n.dart';
 import 'package:fimber/fimber.dart';
 import 'package:integration_test/integration_test.dart';
 
