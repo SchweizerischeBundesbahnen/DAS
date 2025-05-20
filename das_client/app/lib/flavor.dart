c47d50aec187b40f355f72ffaf004ee60a4d9b09
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-05-20 07:48:40 +0200

    chore: formatting

--- das_client/app/lib/flavor.dart
+++ das_client/app/lib/flavor.dart
@@ -7,7 +7,7 @@ enum Flavor {
     displayName: 'Dev',
     tokenExchangeUrl: 'https://sfera-mock.app.sbb.ch/customClaim/requestToken',
     tmsTokenExchangeUrl:
-    'https://imts-token-provider-tms-vad-imtrackside-dev.apps.halon-ocp1-1-t.sbb-aws-test.net/token/exchange',
+        'https://imts-token-provider-tms-vad-imtrackside-dev.apps.halon-ocp1-1-t.sbb-aws-test.net/token/exchange',
     mqttUrl: 'wss://das-poc.messaging.solace.cloud',
     tmsMqttUrl: 'wss://tms-vad-imtrackside-dev-mobile.messaging.solace.cloud',
     authenticatorConfig: _authenticatorConfigMockDev,
@@ -65,7 +65,7 @@ enum Flavor {
 
 const _authenticatorConfigTmsDev = AuthenticatorConfig(
   discoveryUrl:
-  'https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/v2.0/.well-known/openid-configuration',
+      'https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/v2.0/.well-known/openid-configuration',
   clientId: '8af8281c-4f1d-47b5-ad77-526b1da61b2b',
   redirectUrl: 'ch.sbb.das://sbbauth/redirect',
   tokenSpecs: TokenSpecProvider([
