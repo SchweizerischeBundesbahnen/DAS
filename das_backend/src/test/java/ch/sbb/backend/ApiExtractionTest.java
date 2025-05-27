0190349d5ff3a8dd7fe6d53663737b37202f22e6
mghilardelli <marco.ghilardelli@sbb.ch>
2025-05-27 14:41:07 +0200

    test config

--- das_backend/src/test/java/ch/sbb/backend/ApiExtractionTest.java
+++ das_backend/src/test/java/ch/sbb/backend/ApiExtractionTest.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestContainerConfiguration.class)
 @AutoConfigureMockMvc(addFilters = false)
 @Slf4j
