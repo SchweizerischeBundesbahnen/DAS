6b3d2fada3859757f172fe0c8b8962755b8ff7d2
u221711 <thomas.bomatter@sbb.ch>
2025-06-04 15:25:26 +0200

    fix flavor

--- das_client/app/lib/flavor.dart
+++ das_client/app/lib/flavor.dart
@@ -13,7 +13,7 @@ enum Flavor {
     authenticatorConfig: _authenticatorConfigMockDev,
     tmsAuthenticatorConfig: _authenticatorConfigTmsDev,
     backendUrl: 'das-backend-dev.app.sbb.ch',
-    mqttTopicPrefix: 'thomas/',
+    mqttTopicPrefix: 'dev/',
     color: SBBColors.peach,
     showBanner: true,
   ),
