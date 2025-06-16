b82904baf0178833d9d5503de3d57d1166b19012
Marco <marco.ghilardelli@sbb.ch>
2025-06-16 08:44:30 +0200

    feat: formation db model (#715) (#986)

---
+++ das_backend/src/main/java/ch/sbb/backend/formation/infrastructure/trainformation/model/TelTsi.java
@@ -0,0 +1,19 @@
+package ch.sbb.backend.formation.infrastructure.trainformation.model;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * All these elements/attributes are imported from the TAF/TAP XSD
+ * <ul>
+ *  <li>SFERA v3.00 imports TAF/TAP XSD v3.5.0.0</li>
+ *  <li>Further versions will update the import</li>
+ *  <li>The namespace is “teltsi:” (i.e. “Telematics TSI”)</li>
+ * </ul>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TelTsi {
+
+}
