1eb30d21e4af3961a37893e9f3d3bb63d381431b
Raphael Schreiber (IT-PTR-CEN1-BDE22-Extern) <raphael_mathieu.schreiber@sbb.ch>
2025-06-17 16:20:49 +0200

    forgot this todo comment

--- das_client/app/lib/time_controller/punctuality_state_enum.dart
+++ das_client/app/lib/time_controller/punctuality_state_enum.dart
@@ -3,10 +3,3 @@ enum PunctualityState {
   stale,
   hidden,
 }
-/**TODO what to do:
-    - Logic into timecontroller
-    - mock of timecontroller
-    - Unit Test
-    - Add data (no speed story)
-    - darkmode imporvement with next stop and stuff from github
- */
