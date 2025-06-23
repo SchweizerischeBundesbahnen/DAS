4a860bd63f2dc829cb398e605e5730bc2182fa41
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-06-23 08:50:12 +0200

    feat: nav buttons are displayed only in paused and get animation

--- das_client/app/lib/pages/journey/train_journey/widgets/journey_navigation_buttons.dart
+++ das_client/app/lib/pages/journey/train_journey/widgets/journey_navigation_buttons.dart
@@ -1,53 +1,74 @@
 import 'package:app/di/di.dart';
+import 'package:app/pages/journey/navigation/journey_navigation_model.dart';
 import 'package:app/pages/journey/navigation/journey_navigation_view_model.dart';
+import 'package:app/pages/journey/train_journey/widgets/table/config/train_journey_settings.dart';
+import 'package:app/pages/journey/train_journey_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
-class JourneyNavigationButtons extends StatefulWidget {
+const _animationDuration = Duration(milliseconds: 300);
+
+class JourneyNavigationButtons extends StatelessWidget {
   const JourneyNavigationButtons({super.key});
 
   @override
-  State<JourneyNavigationButtons> createState() => _JourneyNavigationButtonsState();
-}
-
-class _JourneyNavigationButtonsState extends State<JourneyNavigationButtons> {
-  @override
   Widget build(BuildContext context) {
-    final viewModel = DI.get<JourneyNavigationViewModel>();
+    final navigationVM = DI.get<JourneyNavigationViewModel>();
+    final journeyVM = context.read<TrainJourneyViewModel>();
     return StreamBuilder(
-      stream: viewModel.model,
-      initialData: viewModel.modelValue,
+      stream: CombineLatestStream.list([navigationVM.model, journeyVM.settings]),
+      initialData: [navigationVM.modelValue, journeyVM.settingsValue],
       builder: (context, snapshot) {
         final snap = snapshot.data;
-        if (snap == null || !snap.showNavigationButtons) return SizedBox.shrink();
+        if (snap == null || snap[0] == null || snap[1] == null) return SizedBox.shrink();
 
-        final isDark = Theme.brightnessOf(context) == Brightness.dark;
+        final navigationModel = snap[0] as JourneyNavigationModel;
+        final settings = snap[1] as TrainJourneySettings;
 
-        return Container(
-          margin: EdgeInsets.only(bottom: sbbDefaultSpacing * 2),
-          padding: EdgeInsets.all(sbbDefaultSpacing / 2),
-          decoration: ShapeDecoration(
-            shape: StadiumBorder(),
-            color: isDark ? SBBColors.granite : SBBColors.milk,
-            shadows: [
-              BoxShadow(
-                blurRadius: sbbDefaultSpacing / 2,
-                color: isDark ? SBBColors.white.withValues(alpha: .4) : SBBColors.black.withValues(alpha: .2),
+        final resolvedShowNavButtons = navigationModel.showNavigationButtons && !settings.isAutoAdvancementEnabled;
+
+        return AnimatedOpacity(
+          opacity: resolvedShowNavButtons ? 1.0 : 0.0,
+          duration: _animationDuration,
+          child: IgnorePointer(
+            ignoring: !resolvedShowNavButtons,
+            child: Container(
+              margin: EdgeInsets.only(bottom: sbbDefaultSpacing * 2),
+              padding: EdgeInsets.all(sbbDefaultSpacing / 2),
+              decoration: _navigationButtonsDecoration(context),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SBBIconButtonLarge(icon: SBBIcons.chevron_left_small, onPressed: () => navigationVM.previous()),
+                  SizedBox(width: sbbDefaultSpacing),
+                  SBBPagination(
+                    numberPages: navigationModel.navigationStackLength,
+                    currentPage: navigationModel.currentIndex,
+                  ),
+                  SizedBox(width: sbbDefaultSpacing),
+                  SBBIconButtonLarge(icon: SBBIcons.chevron_right_small, onPressed: () => navigationVM.next()),
+                ],
               ),
-            ],
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SBBIconButtonLarge(icon: SBBIcons.chevron_left_small, onPressed: () => viewModel.previous()),
-              SizedBox(width: sbbDefaultSpacing),
-              SBBPagination(numberPages: snap.navigationStackLength, currentPage: snap.currentIndex),
-              SizedBox(width: sbbDefaultSpacing),
-              SBBIconButtonLarge(icon: SBBIcons.chevron_right_small, onPressed: () => viewModel.next()),
-            ],
+            ),
           ),
         );
       },
+    );
+  }
+
+  ShapeDecoration _navigationButtonsDecoration(BuildContext context) {
+    final isDark = Theme.brightnessOf(context) == Brightness.dark;
+    return ShapeDecoration(
+      shape: StadiumBorder(),
+      color: isDark ? SBBColors.granite : SBBColors.milk,
+      shadows: [
+        BoxShadow(
+          blurRadius: sbbDefaultSpacing / 2,
+          color: isDark ? SBBColors.white.withValues(alpha: .4) : SBBColors.black.withValues(alpha: .2),
+        ),
+      ],
     );
   }
 }
