ed242e03e0864d80265230a630610a9d26eb64fe
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-07-01 17:24:35 +0200

    feat: advised speed cell displays dash if zero

--- das_client/app/lib/pages/journey/train_journey/widgets/table/cells/advised_speed_cell_body.dart
+++ das_client/app/lib/pages/journey/train_journey/widgets/table/cells/advised_speed_cell_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sfera/component.dart';
 
 class AdvisedSpeedCellBody extends StatelessWidget {
+  static const String _dash = '\u{2013}';
+
   const AdvisedSpeedCellBody({
     required this.speed,
     super.key,
@@ -11,6 +13,7 @@ class AdvisedSpeedCellBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(key: key, speed.value);
+    final resolvedAdvisedSpeed = speed.value == '0' ? _dash : speed.value;
+    return Text(key: key, resolvedAdvisedSpeed);
   }
 }
