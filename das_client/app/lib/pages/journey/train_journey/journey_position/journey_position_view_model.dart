0774b90536d3b5d7c78d977517a36ed6a1fa08cc
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-08-19 09:21:17 +0200

    refactor: small refactor

--- das_client/app/lib/pages/journey/train_journey/journey_position/journey_position_view_model.dart
+++ das_client/app/lib/pages/journey/train_journey/journey_position/journey_position_view_model.dart
@@ -55,7 +55,7 @@ class JourneyPositionViewModel {
 
           final updatedPosition = _calculateCurrentPosition(
             journey.metadata.signaledPosition,
-            journey.journeyPoints ?? [],
+            journey.journeyPoints,
             punctuality,
           );
 
