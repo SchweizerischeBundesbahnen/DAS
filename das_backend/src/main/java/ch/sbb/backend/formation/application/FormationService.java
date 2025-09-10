cdbb46e7da98a1afd5421af00c46c0ae759d5c95
Marco <marco.ghilardelli@sbb.ch>
2025-09-10 11:37:40 +0200

    feat: scheduled clean-up for formation runs (#1176) (#1222)

--- das_backend/src/main/java/ch/sbb/backend/formation/application/FormationService.java
+++ das_backend/src/main/java/ch/sbb/backend/formation/application/FormationService.java
@@ -3,13 +3,24 @@ package ch.sbb.backend.formation.application;
 import ch.sbb.backend.formation.infrastructure.TrainFormationRunRepository;
 import ch.sbb.backend.formation.infrastructure.model.TrainFormationRunEntity;
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 public class FormationService {
 
     private final TrainFormationRunRepository trainFormationRunRepository;
+
+    @Value("${zis.clean-up.older-than-days}")
+    private long cleanUpOlderThanDays;
 
     public FormationService(TrainFormationRunRepository trainFormationRunRepository) {
         this.trainFormationRunRepository = trainFormationRunRepository;
@@ -31,5 +42,15 @@ public class FormationService {
 
     public void deleteByTrainPathIdAndOperationalDay(String trainPathId, LocalDate operationalDay) {
         trainFormationRunRepository.deleteByTrainPathIdAndOperationalDay(trainPathId, operationalDay);
+    }
+
+    @Transactional
+    @Scheduled(cron = "${zis.clean-up.cron}")
+    @SchedulerLock(name = "cleanUpFormations", lockAtLeastFor = "10m")
+    void cleanUpFormations() {
+        LockAssert.assertLocked();
+        OffsetDateTime cleanUpBefore = OffsetDateTime.now().minusDays(cleanUpOlderThanDays);
+        trainFormationRunRepository.deleteByInspectionDateTimeBefore(cleanUpBefore);
+        log.info("Cleaned up formations with inspection before {}", cleanUpBefore);
     }
 }
