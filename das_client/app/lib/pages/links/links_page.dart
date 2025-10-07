d7a8ef7ce88ece30a83ea52bfd5ff31100e71d38
u221638 <ralf.winkelmann2@sbb.ch>
2025-10-07 10:06:54 +0200

    chore: update dependencies and refactor header to use SBB components

--- das_client/app/lib/pages/links/links_page.dart
+++ das_client/app/lib/pages/links/links_page.dart
@@ -2,6 +2,7 @@ import 'package:app/i18n/i18n.dart';
 import 'package:app/nav/das_navigation_drawer.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
 @RoutePage()
@@ -20,6 +21,7 @@ class LinksPage extends StatelessWidget {
   SBBHeader _appBar(BuildContext context) {
     return SBBHeader(
       title: context.l10n.c_app_name,
+      systemOverlayStyle: SystemUiOverlayStyle.light,
     );
   }
 
