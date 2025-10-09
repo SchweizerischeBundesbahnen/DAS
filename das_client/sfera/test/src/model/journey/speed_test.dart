817bf642c5e93684cd416cf99db2ad59fb75bd47
Nicolas Vidoni <30844036+smallTrogdor@users.noreply.github.com>
2025-10-09 11:27:54 +0200

    feat: ignore whitespaces in speeds from TMS VAD #1243 (#1304)

--- das_client/sfera/test/src/model/journey/speed_test.dart
+++ das_client/sfera/test/src/model/journey/speed_test.dart
@@ -9,8 +9,18 @@ void main() {
     );
 
     test(
+      'isValid_whenSingleSpeedWithWhitespace_thenReturnsTrue',
+      () => expect(Speed.isValid(' 80 '), isTrue),
+    );
+
+    test(
       'isValid_whenSingleSpeedWithDecoration_thenReturnsTrue',
       () => expect(Speed.isValid('[90]') && Speed.isValid('{100}'), isTrue),
+    );
+
+    test(
+      'isValid_whenSingleSpeedWithDecorationAndWhitespaces_thenReturnsTrue',
+      () => expect(Speed.isValid('[90 ]') && Speed.isValid('{ 100}'), isTrue),
     );
 
     test(
@@ -19,8 +29,18 @@ void main() {
     );
 
     test(
+      'isValid_whenGraduatedSpeedWithWhitespaces_thenReturnsTrue',
+      () => expect(Speed.isValid('80- 70 -60'), isTrue),
+    );
+
+    test(
       'isValid_whenIncomingOutgoingSpeed_thenReturnsTrue',
       () => expect(Speed.isValid('80/60'), isTrue),
+    );
+
+    test(
+      'isValid_whenIncomingOutgoingSpeedWithWhitespaces_thenReturnsTrue',
+      () => expect(Speed.isValid('80 / 60'), isTrue),
     );
 
     test(
@@ -30,7 +50,7 @@ void main() {
 
     test('isValid_whenInvalidFormat_thenReturnsFalse', () {
       // ARRANGE
-      const inputs = ['5a0', '', '(50)', '80/70/60', '80//60'];
+      const inputs = ['5a0', '', '(50)', '80/70/60', '80//60', '80 // 60'];
 
       // ACT & EXPECT
       for (final input in inputs) {
@@ -40,7 +60,7 @@ void main() {
 
     test('parse_whenSingleSpeedFormat_thenReturnsSingleSpeedType', () {
       // ARRANGE
-      const inputs = ['80', '[90]', '{100}', 'XX', '[XX]', '{XX}'];
+      const inputs = ['80', '[90]', '{100}', 'XX', '[XX]', '{XX}', '{XX }'];
 
       // ACT & EXPECT
       for (final input in inputs) {
@@ -50,7 +70,7 @@ void main() {
 
     test('parse_whenGraduatedSpeedFormat_thenReturnsGraduatedSpeedType', () {
       // ARRANGE
-      const inputs = ['80-70', '80-[70]-60', '{80}-XX-40'];
+      const inputs = ['80-70', '80-[70]-60', '{80}-XX-40', '{80}- XX- 40'];
 
       // ACT & EXPECT
       for (final input in inputs) {
@@ -60,7 +80,7 @@ void main() {
 
     test('parse_whenIncomingOutgoingFormat_thenReturnsIncomingOutgoingSpeedType', () {
       // ARRANGE
-      const inputs = ['80/70', '80-70/60', '80/60-50'];
+      const inputs = ['80/70', '80-70/60', '80/60-50', '80 /60- 50'];
 
       // ACT & EXPECT
       for (final input in inputs) {
