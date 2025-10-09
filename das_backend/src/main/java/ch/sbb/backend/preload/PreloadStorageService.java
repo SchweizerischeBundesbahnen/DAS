28561ee7320df4201e91afae61c10dfd75cb5b83
Fabian Müller <fabian.mueller3@sbb.ch>
2025-10-09 12:43:22 +0200

    add buildZipName-method with OffsetDateTime

--- das_backend/src/main/java/ch/sbb/backend/preload/PreloadStorageService.java
+++ das_backend/src/main/java/ch/sbb/backend/preload/PreloadStorageService.java
@@ -6,7 +6,8 @@ import ch.sbb.backend.adapters.sfera.model.v0201.OTNIDComplexType;
 import ch.sbb.backend.adapters.sfera.model.v0201.SegmentProfile;
 import ch.sbb.backend.adapters.sfera.model.v0201.TrainCharacteristics;
 import ch.sbb.backend.preload.xml.XmlHelper;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Service;
 
@@ -27,14 +28,11 @@ public class PreloadStorageService {
     private static final String ZIP_DIR_SP = DIR_SP + "/";
     private static final String ZIP_DIR_TC = DIR_TC + "/";
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH-mm-ss");
-//    private static final DateTimeFormatter LOCAL_WITH_OFFSET_FILENAME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH-mm-ssXX");
-//    private static final ZoneId ZONE_BERN = ZoneId.of("Europe/Zurich");
-
+    private static final ZoneId ZONE_BERN = ZoneId.of("Europe/Zurich");
+    private static final DateTimeFormatter FILENAME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH-mm-ssXX");
 
     private final XmlHelper xmlHelper;
     private final S3Service s3Service;
-
 
     public PreloadStorageService(XmlHelper xmlHelper, S3Service s3Service) {
         this.xmlHelper = xmlHelper;
@@ -46,13 +44,9 @@ public class PreloadStorageService {
         List<TrainCharacteristics> trainCharacteristics) {
 
         try {
+            String zipName = buildZipName();
 
-            String now = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-            String zipName = now + ".zip";
-//            ZonedDateTime nowLocal = ZonedDateTime.now(ZONE_BERN).withNano(0);
-//            String zipName = LOCAL_WITH_OFFSET_FILENAME_FORMATTER.format(nowLocal) + ".zip";
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(64 * 1024);
             try (ZipOutputStream zos = new ZipOutputStream(baos, StandardCharsets.UTF_8)) {
 
                 addDirectoryEntry(zos, ZIP_DIR_JP);
@@ -68,7 +62,13 @@ public class PreloadStorageService {
 
         } catch (Exception e) {
             throw new RuntimeException("Preload save failed", e);
-        }}
+        }
+    }
+
+    private String buildZipName() {
+        OffsetDateTime nowOffset = OffsetDateTime.now(ZONE_BERN).withNano(0);
+        return FILENAME_FORMATTER.format(nowOffset) + ".zip";
+    }
 
     private void writeJps(List<JourneyProfile> jps, ZipOutputStream zos) throws IOException {
         for (JourneyProfile jp : jps) {
@@ -78,33 +78,31 @@ public class PreloadStorageService {
                 otnid.getTeltsiOperationalTrainNumber(),
                 otnid.getTeltsiStartDate(),
                 jp.getJPVersion());
-            writeXmlFile(zos, filename, jp);
+            writeXmlEntry(zos, filename, jp);
         }
     }
 
     private void writeSps(List<SegmentProfile> sps, ZipOutputStream zos) throws IOException {
         for (SegmentProfile sp : sps) {
-            String filename = String.format("SP_%s_%s_%s_%s.xml",
+            String filename = String.format("SP_%s_%s_%s.xml",
                 sp.getSPID(),
                 sp.getSPVersionMajor(),
-                sp.getSPVersionMinor(),
-                sp.getSPZone().getIMID());
-            writeXmlFile(zos,  filename, sp);
+                sp.getSPVersionMinor());
+            writeXmlEntry(zos,  filename, sp);
         }
     }
 
     private void writeTcs(List<TrainCharacteristics> tcs, ZipOutputStream zos) throws IOException {
         for (TrainCharacteristics tc : tcs) {
-            String filename = String.format("TC_%s_%s_%s_%s.xml",
+            String filename = String.format("TC_%s_%s_%s.xml",
                 tc.getTCID(),
                 tc.getTCVersionMajor(),
-                tc.getTCVersionMinor(),
-                tc.getTCRUID());
-            writeXmlFile(zos, filename, tc);
+                tc.getTCVersionMinor());
+            writeXmlEntry(zos, filename, tc);
         }
     }
 
-    private void writeXmlFile(ZipOutputStream zos, String entryName, Object obj) throws IOException {
+    private void writeXmlEntry(ZipOutputStream zos, String entryName, Object obj) throws IOException {
         if (obj == null) return;
         String xml = xmlHelper.toString(obj);
         ZipEntry entry = new ZipEntry(entryName);
