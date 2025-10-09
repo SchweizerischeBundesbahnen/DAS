73d31b62db0c62a3982eea8d09f7ef10e96ace04
Peter <hipsand@gmail.com>
2025-10-09 13:25:41 +0200

    missing Modulith pre-requisite table added

---
+++ das_backend/src/main/resources/db/migration/V1.8__add_event_publication_table.sql
@@ -0,0 +1,13 @@
+-- Modulith pre-requisite, see https://docs.spring.io/spring-modulith/reference/appendix.html#schemas.postgresql
+CREATE TABLE IF NOT EXISTS event_publication
+(
+  id               UUID NOT NULL,
+  listener_id      TEXT NOT NULL,
+  event_type       TEXT NOT NULL,
+  serialized_event TEXT NOT NULL,
+  publication_date TIMESTAMP WITH TIME ZONE NOT NULL,
+  completion_date  TIMESTAMP WITH TIME ZONE,
+  PRIMARY KEY (id)
+);
+CREATE INDEX IF NOT EXISTS event_publication_serialized_event_hash_idx ON event_publication USING hash(serialized_event);
+CREATE INDEX IF NOT EXISTS event_publication_by_completion_date_idx ON event_publication (completion_date);
