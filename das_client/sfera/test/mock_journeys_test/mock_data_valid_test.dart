9236b45cc56194e319946bae7c268eb7e635f547
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-10-29 13:57:24 +0100

    chore(test): add logging with print to mock_data_valid_test.dart

--- das_client/sfera/test/mock_journeys_test/mock_data_valid_test.dart
+++ das_client/sfera/test/mock_journeys_test/mock_data_valid_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
 
 import '../util/test_journey_loader.dart';
 
@@ -12,10 +13,20 @@ void main() {
     expect(testResourcesDir.existsSync(), isTrue);
   });
 
+  group('validatingJourneys', () {
+    setUpAll(() {
+      Logger.root.level = Level.WARNING;
+      Logger.root.onRecord.listen((record) {
+        print('${record.level.name}: ${record.time}: ${record.message}');
+      });
+    });
+  });
+
   group('whenLoadingAllJourneysFromSferaTestResourcesDir_thenShould', () {
     for (final testJourney in TestJourneyLoader.fromStaticSferaResources()) {
       final journeyName = [testJourney.name, testJourney.eventName].join('-');
       test('whenParsingJourney_${journeyName}_thenShouldBeValid', tags: 'sfera-mock-data-validator', () {
+        expect(testJourney.validate(), isTrue);
         expect(testJourney.journey.valid, isTrue);
       });
     }
@@ -25,6 +36,7 @@ void main() {
     for (final testJourney in TestJourneyLoader.fromClientTestResources()) {
       final journeyName = [testJourney.name, testJourney.eventName].nonNulls.join('-');
       test('whenParsingJourney_${journeyName}_thenShouldBeValid', tags: 'sfera-mock-data-validator', () {
+        expect(testJourney.validate(), isTrue);
         expect(testJourney.journey.valid, isTrue);
       });
     }
