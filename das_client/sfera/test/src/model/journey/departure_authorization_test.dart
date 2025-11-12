d42c507951939ab9f0af45b4d8651915f211c453
u221638 <ralf.winkelmann2@sbb.ch>
2025-11-12 17:17:20 +0100

    chore: added unit tests

---
+++ das_client/sfera/test/src/model/journey/departure_authorization_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sfera/component.dart';
+
+void main() {
+  test('text_whenNoTypeAndText_thenReturnsNull', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(types: [], originalText: null);
+
+    // WHEN THEN
+    expect(departureAuth.text, isNull);
+  });
+
+  test('text_whenDispatcherTypeAndNoText_thenReturnsPrefix', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(types: [DepartureAuthorizationType.dispatcher], originalText: null);
+
+    // WHEN THEN
+    expect(departureAuth.text, '*');
+  });
+
+  test('text_whenDispatcherTypeAndText_thenReturnsPrefixedText', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(types: [DepartureAuthorizationType.dispatcher], originalText: '10-30');
+
+    // WHEN THEN
+    expect(departureAuth.text, '* 10-30');
+  });
+
+  test('text_whenSmsTypeAndText_thenReturnsText', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(types: [DepartureAuthorizationType.sms], originalText: 'sms 3-6');
+
+    // WHEN THEN
+    expect(departureAuth.text, 'sms 3-6');
+  });
+
+  test('text_whenSmsAndDispatcherTypeAndText_thenReturnsText', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(
+      types: [DepartureAuthorizationType.sms, DepartureAuthorizationType.dispatcher],
+      originalText: 'sms 3-6',
+    );
+
+    // WHEN THEN
+    expect(departureAuth.text, '* sms 3-6');
+  });
+
+  test('text_whenTextWithLineBreaks_thenReturnsTextWithSpaces', () {
+    // GIVEN
+    final departureAuth = DepartureAuthorization(
+      types: [DepartureAuthorizationType.sms],
+      originalText: 'sms<br/>3-6\n',
+    );
+
+    // WHEN THEN
+    expect(departureAuth.text, 'sms 3-6 ');
+  });
+}
