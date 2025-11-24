a7e5facf6eae6407e0bd58d497b68a172550b360
mghilardelli <marco.ghilardelli@sbb.ch>
2025-11-24 08:34:47 +0100

    feat: i18n initialization

--- das_admin_tool/src/app/app.routes.ts
+++ das_admin_tool/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import {Routes} from '@angular/router';
 import {Home} from './home/home';
+import {Page} from './page/page';
 
 export const routes: Routes = [
+  {
+    path: 'page',
+    component: Page
+  },
   {
     path: '',
     component: Home
