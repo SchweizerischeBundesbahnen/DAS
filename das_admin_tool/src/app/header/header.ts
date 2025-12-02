4bf6ec6acef576913635f35d49666a907abf97ab
Marco <marco.ghilardelli@sbb.ch>
2025-12-02 11:08:45 +0100

    feat: authentication (#212) (#1451)

--- das_admin_tool/src/app/header/header.ts
+++ das_admin_tool/src/app/header/header.ts
@@ -1,8 +1,11 @@
-import {Component, inject, LOCALE_ID} from '@angular/core';
+import {Component, computed, inject, LOCALE_ID} from '@angular/core';
 import {SbbHeader, SbbHeaderButton, SbbHeaderEnvironment} from "@sbb-esta/lyne-angular/header";
-import {SbbMenu, SbbMenuLink} from "@sbb-esta/lyne-angular/menu";
+import {SbbMenu, SbbMenuButton, SbbMenuLink} from "@sbb-esta/lyne-angular/menu";
 import {environment} from '../../environments/environment';
 import packageJson from '../../../package.json';
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {SbbDivider} from '@sbb-esta/lyne-angular/divider';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +14,15 @@ import packageJson from '../../../package.json';
     SbbHeaderButton,
     SbbHeaderEnvironment,
     SbbMenu,
-    SbbMenuLink
+    SbbMenuLink,
+    SbbDivider,
+    SbbMenuButton
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  protected readonly oidcSecurityService = inject(OidcSecurityService);
   protected readonly stage = environment.stage;
   protected readonly version = packageJson.version;
   protected readonly languages = [
@@ -24,6 +30,8 @@ export class Header {
     {localeId: 'fr-CH', path: 'fr', label: 'Français'},
     {localeId: 'it-CH', path: 'it', label: 'Italiano'}
   ];
+  protected readonly roles = computed(() => this.oidcSecurityService.userData().userData.roles.filter((role: string) => role.includes('admin')));
+  private readonly router = inject(Router);
   private readonly localeId = inject(LOCALE_ID);
   protected readonly currentLanguage = this.languages
     .find(lang => lang.localeId === this.localeId);
@@ -35,5 +43,18 @@ export class Header {
     }
     segments[0] = languagePath;
     window.location.href = '/' + segments.join('/') + window.location.search + window.location.hash;
+  }
+
+  protected login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  protected switchLogin() {
+    this.oidcSecurityService.authorize(undefined, {customParams: {prompt: 'select_account'}})
+  }
+
+  protected async logout() {
+    this.oidcSecurityService.logoffLocalMultiple();
+    return this.router.navigate(['/unauthorized']);
   }
 }
