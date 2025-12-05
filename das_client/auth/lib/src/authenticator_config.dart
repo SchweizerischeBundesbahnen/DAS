334d8650c5632474516c614c162a80c149aa30b6
DevCore <62793354+rawi-coding@users.noreply.github.com>
2025-12-05 13:54:13 +0100

    feat: validate trusted tenant for auth (#1148) (#1508)

--- das_client/auth/lib/src/authenticator_config.dart
+++ das_client/auth/lib/src/authenticator_config.dart
@@ -1,6 +1,10 @@
 import 'package:auth/src/token_spec_provider.dart';
 import 'package:meta/meta.dart';
 
+const sbbTenantId = '2cda5d11-f0ac-46b3-967d-af1b2e1bd01a';
+const blsTenantId = 'd653d01f-17a4-48a1-9aab-b780b61b4273';
+const sobTenantId = 'a64ce5df-4ad8-40b9-91ee-54bac2bb8326';
+
 @sealed
 @immutable
 class AuthenticatorConfig {
@@ -10,6 +14,7 @@ class AuthenticatorConfig {
     required this.redirectUrl,
     required this.tokenSpecs,
     this.postLogoutRedirectUrl,
+    this.trustedTenantIds = const [sbbTenantId, blsTenantId, sobTenantId],
   });
 
   const AuthenticatorConfig.empty()
@@ -17,13 +22,17 @@ class AuthenticatorConfig {
       clientId = '',
       redirectUrl = '',
       postLogoutRedirectUrl = null,
-      tokenSpecs = const TokenSpecProvider.empty();
+      tokenSpecs = const TokenSpecProvider.empty(),
+      trustedTenantIds = const [];
 
   final String discoveryUrl;
   final String clientId;
   final String redirectUrl;
   final String? postLogoutRedirectUrl;
   final TokenSpecProvider tokenSpecs;
+
+  /// list of trusted tenants that are validated in token claim
+  final List<String> trustedTenantIds;
 
   @override
   bool operator ==(Object other) {
