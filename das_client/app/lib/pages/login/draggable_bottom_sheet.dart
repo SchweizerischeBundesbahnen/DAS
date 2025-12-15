fbd6c963d8dbcc2da5e303991b13dbd015671b0a
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-12-15 23:38:38 +0100

    feat: more sophisticated UI is done

---
+++ das_client/app/lib/pages/login/draggable_bottom_sheet.dart
@@ -0,0 +1,208 @@
+import 'dart:math' show min;
+
+import 'package:app/di/di.dart';
+import 'package:app/di/scope_handler.dart';
+import 'package:app/di/scopes/journey_scope.dart';
+import 'package:app/flavor.dart';
+import 'package:app/i18n/src/build_context_x.dart';
+import 'package:app/nav/app_router.dart';
+import 'package:app/theme/theme_util.dart';
+import 'package:app/util/device_screen.dart';
+import 'package:app/widgets/das_text_styles.dart';
+import 'package:auth/component.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
+
+final _log = Logger('LoginDraggableBottomSheet');
+
+class LoginDraggableBottomSheet extends StatefulWidget {
+  @override
+  State<LoginDraggableBottomSheet> createState() => _LoginDraggableBottomSheetState();
+
+  static const _minHeight = 116.0;
+
+  const LoginDraggableBottomSheet({super.key});
+}
+
+class _LoginDraggableBottomSheetState extends State<LoginDraggableBottomSheet> {
+  final _controller = DraggableScrollableController();
+  final minHeight = min(LoginDraggableBottomSheet._minHeight / DeviceScreen.size.height, 0.5);
+  final flavor = DI.get<Flavor>();
+
+  final _packageInfo = PackageInfo.fromPlatform();
+
+  bool isTmsChecked = false;
+  bool isLoading = false;
+
+  String? errorText;
+
+  @override
+  void initState() {
+    DI.resetToUnauthenticatedScope(useTms: isTmsChecked);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<PackageInfo>(
+      future: _packageInfo,
+      builder: (context, asyncSnapshot) {
+        final packageInfo = asyncSnapshot.data;
+
+        if (packageInfo == null) return SizedBox.shrink();
+
+        return DraggableScrollableSheet(
+          controller: _controller,
+          initialChildSize: minHeight,
+          maxChildSize: 0.5,
+          minChildSize: minHeight,
+          builder: (context, controller) => Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(sbbDefaultSpacing),
+                  topRight: Radius.circular(sbbDefaultSpacing),
+                ),
+              ),
+              shadows: [
+                BoxShadow(
+                  color: SBBColors.black.withValues(alpha: 0.2),
+                  blurRadius: 8,
+                ),
+              ],
+              color: ThemeUtil.getColor(context, SBBColors.milk, SBBColors.black),
+            ),
+            child: CustomScrollView(
+              controller: controller,
+              slivers: [
+                PinnedHeaderSliver(child: _body(context)),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(vertical: sbbDefaultSpacing, horizontal: sbbDefaultSpacing * .5),
+                  sliver: SliverToBoxAdapter(
+                    child: Column(
+                      crossAxisAlignment: .start,
+                      children: [
+                        SBBGroup(
+                          child: SBBSwitchListItem(
+                            title: context.l10n.p_login_connect_to_tms,
+                            value: isTmsChecked,
+                            onChanged: (value) {
+                              setState(() {
+                                isTmsChecked = value;
+                                DI.resetToUnauthenticatedScope(useTms: isTmsChecked);
+                              });
+                            },
+                            isLastElement: true,
+                          ),
+                        ),
+                        SizedBox(height: 32.0),
+                        RichText(
+                          text: TextSpan(
+                            text: 'App Flavor: ',
+                            style: DASTextStyles.smallLight.copyWith(color: SBBColors.granite),
+                            children: [
+                              TextSpan(
+                                text: flavor.displayName,
+                                style: DASTextStyles.smallBold.copyWith(color: SBBColors.granite),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'App Version: ${packageInfo.version}+${packageInfo.buildNumber}',
+                          style: DASTextStyles.smallLight.copyWith(color: SBBColors.granite),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(56, 24, 32, 32),
+      color: SBBColors.white,
+      child: Row(
+        children: [
+          Expanded(child: _message(context)),
+          _loginButton(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _message(BuildContext context) {
+    return Column(
+      mainAxisSize: .min,
+      spacing: 8.0,
+      children: [
+        if (errorText == null) ...[
+          Text(context.l10n.p_login_bottom_sheet_title, style: sbbTextStyle.boldStyle.xLarge),
+          Text(context.l10n.p_login_bottom_sheet_subtitle),
+        ],
+        if (errorText != null) ...[
+          Row(
+            mainAxisSize: .min,
+            children: [
+              Icon(SBBIcons.circle_cross_small, color: SBBColors.red),
+              SizedBox(width: sbbDefaultSpacing * .5),
+              Text(context.l10n.p_login_bottom_sheet_title_failed, style: sbbTextStyle.boldStyle.xLarge),
+            ],
+          ),
+          Text('${context.l10n.p_login_bottom_sheet_subtitle_failed}: $errorText'),
+        ],
+      ],
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    if (errorText != null) {
+      return SBBIconButtonLarge(icon: SBBIcons.arrow_circle_reset_medium, onPressed: () => _onLoginPressed(context));
+    }
+
+    // TODO: change to SBBSecondaryButton with custom label once v5.0.0 is released
+    // TODO: https://github.com/SchweizerischeBundesbahnen/design_system_flutter/pull/425
+    return OutlinedButton(
+      onPressed: isLoading ? null : () => _onLoginPressed(context),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Text(context.l10n.p_login_login_button_text),
+      ),
+    );
+  }
+
+  void _onLoginPressed(BuildContext context) async {
+    final authenticator = DI.get<Authenticator>();
+
+    setState(() {
+      isLoading = true;
+      errorText = null;
+    });
+
+    try {
+      await authenticator.login();
+      await DI.get<ScopeHandler>().push<AuthenticatedScope>();
+      await DI.get<ScopeHandler>().push<JourneyScope>();
+      if (context.mounted) {
+        context.router.replace(const JourneySelectionRoute());
+      }
+    } catch (e) {
+      _log.severe('Login failed', e);
+      setState(() {
+        isLoading = false;
+        errorText = e.toString();
+      });
+    }
+  }
+}
--- das_client/app/lib/pages/login/login_page.dart
+++ das_client/app/lib/pages/login/login_page.dart
@@ -1,39 +1,17 @@
-import 'package:app/di/di.dart';
-import 'package:app/di/scope_handler.dart';
-import 'package:app/di/scopes/journey_scope.dart';
-import 'package:app/flavor.dart';
-import 'package:app/i18n/i18n.dart';
-import 'package:app/nav/app_router.dart';
-import 'package:app/theme/theme_util.dart';
+import 'package:app/pages/login/draggable_bottom_sheet.dart';
 import 'package:app/widgets/assets.dart';
-import 'package:auth/component.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logging/logging.dart';
-import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
 final _log = Logger('LoginPage');
 
 @RoutePage()
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   static const routeName = 'login';
 
   const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  bool isLoading = false;
-  bool isTmsChecked = false;
-
-  @override
-  void initState() {
-    DI.resetToUnauthenticatedScope(useTms: isTmsChecked);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
         alignment: .bottomCenter,
         children: [
           _background(),
-          _bottomSheet(context, child: isLoading ? _loading() : _body()),
+          LoginDraggableBottomSheet(),
         ],
       ),
     );
@@ -54,136 +32,4 @@ class _LoginPageState extends State<LoginPage> {
     width: double.infinity,
     height: double.infinity,
   );
-
-  Widget _bottomSheet(BuildContext context, {required Widget child}) {
-    return Container(
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(sbbDefaultSpacing),
-            topRight: Radius.circular(sbbDefaultSpacing),
-          ),
-        ),
-        shadows: [
-          BoxShadow(
-            color: SBBColors.black.withValues(alpha: 0.2),
-            blurRadius: 8,
-          ),
-        ],
-        color: ThemeUtil.getColor(context, SBBColors.white, SBBColors.charcoal),
-      ),
-      child: child,
-    );
-  }
-
-  Widget _loading() {
-    return Container(
-      alignment: .center,
-      child: const CircularProgressIndicator(),
-    );
-  }
-
-  Widget _body() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(56, 24, 32, 32),
-      child: Row(
-        children: [
-          Expanded(child: _message(context)),
-          // TODO: think of a way to display this information
-          // _tmsCheckbox(context),
-          // _flavor(context),
-          _loginButton(context),
-        ],
-      ),
-    );
-  }
-
-  Widget _message(BuildContext context) {
-    return Column(
-      mainAxisSize: .min,
-      spacing: 8.0,
-      children: [
-        Text(context.l10n.p_login_bottom_sheet_title, style: sbbTextStyle.boldStyle.xLarge),
-        Text(context.l10n.p_login_bottom_sheet_subtitle),
-      ],
-    );
-  }
-
-  Widget _flavor(BuildContext context) {
-    final flavor = DI.get<Flavor>();
-    return Container(
-      alignment: .center,
-      padding: const .all(16.0),
-      child: Text(
-        'Flavor: ${flavor.displayName}',
-      ),
-    );
-  }
-
-  Widget _tmsCheckbox(BuildContext context) {
-    final flavor = DI.get<Flavor>();
-    if (!flavor.isTmsEnabledForFlavor) return SizedBox.shrink();
-
-    return Padding(
-      padding: const .all(sbbDefaultSpacing),
-      child: Row(
-        mainAxisSize: .min,
-        children: [
-          SBBCheckbox(
-            value: isTmsChecked,
-            onChanged: (value) {
-              setState(() {
-                isTmsChecked = value ?? false;
-                DI.resetToUnauthenticatedScope(useTms: isTmsChecked);
-              });
-            },
-          ),
-          Text(context.l10n.p_login_connect_to_tms),
-        ],
-      ),
-    );
-  }
-
-  Widget _loginButton(BuildContext context) {
-    // TODO: change to SBBSecondaryButton with custom label once v5.0.0 is released
-    // TODO: https://github.com/SchweizerischeBundesbahnen/design_system_flutter/pull/425
-    return OutlinedButton(
-      onPressed: _onLoginPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Text(context.l10n.p_login_login_button_text),
-      ),
-    );
-  }
-
-  void _onLoginPressed() async {
-    final authenticator = DI.get<Authenticator>();
-
-    setState(() {
-      isLoading = true;
-    });
-
-    final context = this.context;
-    try {
-      await authenticator.login();
-      await DI.get<ScopeHandler>().push<AuthenticatedScope>();
-      await DI.get<ScopeHandler>().push<JourneyScope>();
-      if (context.mounted) {
-        context.router.replace(const JourneySelectionRoute());
-      }
-    } catch (e) {
-      _log.severe('Login failed', e);
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(context.l10n.p_login_login_failed),
-          ),
-        );
-      }
-    }
-
-    setState(() {
-      isLoading = false;
-    });
-  }
 }
