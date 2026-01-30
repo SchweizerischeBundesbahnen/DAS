ee1f4b8e4b4d213f38b88d7227a06c62b30d5414
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-01-30 09:10:55 +0100

    feat: make button correct size

--- das_client/app/lib/pages/journey/break_load_slip/widgets/break_load_slip_replace_break_series_button.dart
+++ das_client/app/lib/pages/journey/break_load_slip/widgets/break_load_slip_replace_break_series_button.dart
@@ -3,7 +3,6 @@ import 'package:app/pages/journey/break_load_slip/break_load_slip_view_model.dar
 import 'package:app/util/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
 class BreakLoadSlipReplaceBreakSeriesButton extends StatelessWidget {
   const BreakLoadSlipReplaceBreakSeriesButton({super.key});
@@ -17,17 +16,13 @@ class BreakLoadSlipReplaceBreakSeriesButton extends StatelessWidget {
       builder: (context, asyncSnapshot) {
         Widget child = SizedBox.shrink();
 
-        // TODO: change to SBBSecondaryButton with custom style once v5.0.0 is released
+        // TODO: change to SBBSecondaryButton with custom label once v5.0.0 is released
         // TODO: https://github.com/SchweizerischeBundesbahnen/design_system_flutter/pull/425
         if (viewModel.isActiveFormationRun) {
           child = OutlinedButton(
             onPressed: viewModel.isJourneyAndActiveFormationRunBreakSeriesDifferent()
                 ? () => viewModel.updateJourneyBreakSeriesFromActiveFormationRun()
                 : null,
-            style: ButtonStyle(
-              minimumSize: WidgetStatePropertyAll(Size(0.0, SBBSpacing.xLarge)),
-              fixedSize: WidgetStatePropertyAll(Size.infinite),
-            ),
             child: Text(context.l10n.p_break_load_slip_button_apply_train_series),
           );
         }
