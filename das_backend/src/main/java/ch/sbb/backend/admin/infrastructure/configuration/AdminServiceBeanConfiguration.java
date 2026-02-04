a7a4011bcaf20af05299360bd4cd530cc3018884
Marco <marco.ghilardelli@sbb.ch>
2026-02-04 09:53:31 +0100

    chore: remove service points (#1687)

--- das_backend/src/main/java/ch/sbb/backend/admin/infrastructure/configuration/AdminServiceBeanConfiguration.java
+++ das_backend/src/main/java/ch/sbb/backend/admin/infrastructure/configuration/AdminServiceBeanConfiguration.java
@@ -1,8 +1,5 @@
 package ch.sbb.backend.admin.infrastructure.configuration;
 
-import ch.sbb.backend.admin.domain.servicepoint.ServicePointRepository;
-import ch.sbb.backend.admin.domain.servicepoint.ServicePointService;
-import ch.sbb.backend.admin.domain.servicepoint.ServicePointServiceImpl;
 import ch.sbb.backend.admin.domain.settings.RuFeatureRepository;
 import ch.sbb.backend.admin.domain.settings.RuFeatureService;
 import ch.sbb.backend.admin.domain.settings.RuFeatureServiceImpl;
@@ -11,11 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AdminServiceBeanConfiguration {
-
-    @Bean
-    ServicePointService servicePointService(ServicePointRepository servicePointRepository) {
-        return new ServicePointServiceImpl(servicePointRepository);
-    }
 
     @Bean
     RuFeatureService ruFeatureService(RuFeatureRepository ruFeatureRepository) {
