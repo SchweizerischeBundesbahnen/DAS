1dac5deb57a15357c6697a1d61bab79ccf5602b5
Fabian Müller <fabian.mueller3@sbb.ch>
2026-02-09 13:32:48 +0100

    fix: add API for client #1406

--- das_backend/src/main/java/ch/sbb/backend/admin/application/settings/ConfigService.java
+++ das_backend/src/main/java/ch/sbb/backend/admin/application/settings/ConfigService.java
@@ -1,9 +1,7 @@
 package ch.sbb.backend.admin.application.settings;
 
-import ch.sbb.backend.admin.application.settings.model.response.AppVersion;
 import ch.sbb.backend.admin.application.settings.model.response.Logging;
 import ch.sbb.backend.admin.application.settings.model.response.Preload;
-import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -25,24 +23,11 @@ public class ConfigService {
     @Value("${preload.bucket.accessSecret}")
     private String accessSecret;
 
-    @Value("")
-    private Boolean updateRequired;
-
-    @Value("")
-    private LocalDate expiryDate;
-
-    @Value("")
-    private String currentVersion;
-
     public Logging getLogging() {
         return new Logging(url, token);
     }
 
     public Preload getPreload() {
         return new Preload(bucketUrl, accessKey, accessSecret);
-    }
-
-    public AppVersion getAppVersion() {
-        return new AppVersion(updateRequired, expiryDate, currentVersion);
     }
 }
