17820ac07753d308da955a725e1de9e572472264
Thomas Bomatter <thomas.bomatter@sbb.ch>
2026-02-13 10:41:32 +0100

    renaming

--- das_client/sfera/test/mock_journeys_test/mock_data_valid_test.dart
+++ das_client/sfera/test/mock_journeys_test/mock_data_valid_test.dart
@@ -8,7 +8,7 @@ import '../util/test_journey/test_journey_repository.dart';
 void main() {
   const sferaStaticResourcesDirectoryPath = '../../sfera_mock/src/main/resources/static_sfera_resources';
   final testResourcesDir = Directory(sferaStaticResourcesDirectoryPath);
-  final invalidJourney = ['T34'];
+  final invalidJourneys = ['T34'];
 
   test('whenSferaStaticResourcesDirPath_thenShouldFindDirectory', tags: 'sfera-mock-data-validator', () {
     expect(testResourcesDir.existsSync(), isTrue);
@@ -28,7 +28,7 @@ void main() {
         test('whenParsingJourney_${journeyName}_thenShouldBeValid', tags: 'sfera-mock-data-validator', () {
           expect(testJourney.validate(), isTrue, reason: 'Expected $journeyName to be valid!');
 
-          if (invalidJourney.any((it) => journeyName.contains(it))) {
+          if (invalidJourneys.any((it) => journeyName.contains(it))) {
             expect(testJourney.journey.valid, isFalse, reason: 'Expected $journeyName to be invalid!');
           } else {
             expect(testJourney.journey.valid, isTrue, reason: 'Expected $journeyName to be valid!');
