1ab508f8bd24ea7cc639c5c62c3d0db9f1d58c4a
mghilardelli <marco.ghilardelli@sbb.ch>
2026-02-20 14:52:36 +0100

    feat: preload SPs in chunks (#1653)

--- das_backend/src/main/java/ch/sbb/backend/preload/application/model/trainidentification/TrainIdentification.java
+++ das_backend/src/main/java/ch/sbb/backend/preload/application/model/trainidentification/TrainIdentification.java
@@ -1,22 +1,27 @@
 package ch.sbb.backend.preload.application.model.trainidentification;
 
+import ch.sbb.backend.preload.infrastructure.xml.XmlDateHelper;
+import ch.sbb.backend.preload.sfera.model.v0300.JPRequest;
+import ch.sbb.backend.preload.sfera.model.v0300.OTNID;
 import java.time.LocalDate;
 import java.util.Set;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NonNull;
 
-@Builder
-@Getter
-public class TrainIdentification {
+public record TrainIdentification(@NonNull Integer id, @NonNull String operationalTrainNumber, @NonNull LocalDate startDate, @NonNull Set<CompanyCode> companies) {
 
-    @NonNull
-    String operationalTrainNumber;
+    public CompanyCode company() {
+        return companies.stream().findFirst().orElseThrow(() -> new IllegalStateException("TrainIdentification must have at least one company"));
+    }
 
-    @NonNull
-    LocalDate startDate;
-
-    @NonNull
-    @Builder.Default
-    Set<CompanyCode> companies = Set.of();
+    public JPRequest toJpRequest() {
+        JPRequest result = new JPRequest();
+        ch.sbb.backend.preload.sfera.model.v0300.TrainIdentification trainIdentification = new ch.sbb.backend.preload.sfera.model.v0300.TrainIdentification();
+        OTNID otnid = new OTNID();
+        otnid.setTeltsiCompany(company().getValue());
+        otnid.setTeltsiOperationalTrainNumber(operationalTrainNumber);
+        otnid.setTeltsiStartDate(XmlDateHelper.toGregorianCalender(startDate));
+        trainIdentification.setOTNID(otnid);
+        result.setTrainIdentification(trainIdentification);
+        return result;
+    }
 }
