a6cbb8c97464e20fc703b1409ce0985a9a2864c8
mghilardelli <marco.ghilardelli@sbb.ch>
2026-03-02 17:12:47 +0100

    feat: authorize requests (#428)

--- das_backend/src/main/java/ch/sbb/backend/common/config/WebSecurityConfig.java
+++ das_backend/src/main/java/ch/sbb/backend/common/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package ch.sbb.backend.common.config;
 
+import static ch.sbb.backend.admin.application.settings.SettingsController.API_SETTINGS;
+import static ch.sbb.backend.formation.api.v1.FormationController.API_FORMATIONS;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,12 +16,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Production security.
+ *
  * @see JWTConfig
  */
 @Configuration
 @Profile("!local-no-docker")
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
+
+    public static final String ROLE_OBSERVER = "observer";
+    public static final String ROLE_DRIVER = "driver";
+    public static final String ROLE_RU_ADMIN = "ru_admin";
+    public static final String ROLE_ADMIN = "admin";
 
     @Autowired
     JwtAuthenticationConverter jwtAuthenticationConverter;
@@ -25,19 +36,16 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/actuator/health/**").permitAll()
+                .requestMatchers(API_SETTINGS, API_FORMATIONS).hasAnyRole(ROLE_OBSERVER, ROLE_DRIVER)
                 .anyRequest().authenticated()
             )
             .csrf(AbstractHttpConfigurer::disable)
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter))
             );
-
         return http.build();
     }
-
-
 }
 
