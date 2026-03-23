66dd9d863ce27e4d15ce9f1d9b0ee2b8827c6e90
u221638 <ralf.winkelmann2@sbb.ch>
2026-03-23 16:09:21 +0100

    fix: login page background on Android

--- das_client/app/lib/widgets/assets.dart
+++ das_client/app/lib/widgets/assets.dart
@@ -53,7 +53,7 @@ class AppAssets {
   static const shapeRoundedEdgeLeftSmall = '$_othersDir/shape_rounded_edge_left_small.svg';
   static const shapeRoundedEdgeLeftMedium = '$_othersDir/shape_rounded_edge_left_medium.svg';
   static const shapeRoundedEdgeLeftLarge = '$_othersDir/shape_rounded_edge_left_large.svg';
-  static const loginPageBackground = '$_othersDir/login_page_background.svg';
+  static const loginPageBackground = '$_othersDir/login_page_background.png';
   static const stationSignBhf = '$_othersDir/station_sign_bhf.svg';
 
   // audio
