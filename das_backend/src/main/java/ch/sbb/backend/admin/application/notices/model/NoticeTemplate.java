90fd0c9ad3cbdfcd8a20104fb27a86336d0a2532
mghilardelli <marco.ghilardelli@sbb.ch>
2026-04-28 09:20:21 +0200

    api spec

--- das_backend/src/main/java/ch/sbb/backend/admin/application/notices/model/NoticeTemplate.java
+++ das_backend/src/main/java/ch/sbb/backend/admin/application/notices/model/NoticeTemplate.java
@@ -1,12 +1,13 @@
 package ch.sbb.backend.admin.application.notices.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.time.LocalDateTime;
 
 @Schema(description = "Notice template payload. At least one language object (de, fr, or it) is provided.")
 public record NoticeTemplate(
-    @Schema(description = "The unique identifier for the notice template entry.", requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "The unique identifier for the notice template entry.", requiredMode = RequiredMode.REQUIRED, accessMode = AccessMode.READ_ONLY)
     Integer id,
     @Schema(description = "The category of the notice template.", requiredMode = RequiredMode.REQUIRED)
     String category,
@@ -16,10 +17,14 @@ public record NoticeTemplate(
     NoticeTemplateLanguage fr,
     @Schema(description = "The italian notice template.", requiredMode = RequiredMode.NOT_REQUIRED)
     NoticeTemplateLanguage it,
-    @Schema(description = "The user who created or last updated the notice template.", requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "The user who created or last updated the notice template.", requiredMode = RequiredMode.REQUIRED, accessMode = AccessMode.READ_ONLY)
     String lastModifiedBy,
-    @Schema(description = "The timestamp of when the notice template was created or last updated.", requiredMode = RequiredMode.REQUIRED)
+    @Schema(description = "The timestamp of when the notice template was created or last updated.", requiredMode = RequiredMode.REQUIRED, accessMode = AccessMode.READ_ONLY)
     LocalDateTime lastModifiedAt
 ) {
+
+    public NoticeTemplate(Integer id, String category, NoticeTemplateLanguage de, NoticeTemplateLanguage fr, NoticeTemplateLanguage it) {
+        this(id, category, de, fr, it, null, null);
+    }
 
 }
