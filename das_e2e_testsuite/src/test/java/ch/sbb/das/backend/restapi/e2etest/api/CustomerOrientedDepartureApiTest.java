3eb5c5ed74f245fc21d5016010b5be857147b79e
u210875 <peter.hirzel@sbb.ch>
2026-05-09 10:44:48 +0200

    update params to das_backend v0.11.0

--- das_e2e_testsuite/src/test/java/ch/sbb/das/backend/restapi/e2etest/api/CustomerOrientedDepartureApiTest.java
+++ das_e2e_testsuite/src/test/java/ch/sbb/das/backend/restapi/e2etest/api/CustomerOrientedDepartureApiTest.java
@@ -6,8 +6,11 @@ import ch.sbb.das.backend.restapi.configuration.DasBackendApi;
 import ch.sbb.das.backend.restapi.configuration.DasBackendEndpointConfiguration;
 import ch.sbb.das.backend.restapi.e2etest.configuration.ApiClientTestProfile;
 import ch.sbb.das.backend.restapi.e2etest.helper.RestAssuredCommand;
-import ch.sbb.das.backend.restapi.monitoring.MonitoringConstants;
+import ch.sbb.das.backend.restclient.v1.model.SubscribeRequest;
 import io.restassured.response.Response;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +23,18 @@ import org.springframework.test.context.TestContextManager;
 class CustomerOrientedDepartureApiTest extends RestAssuredCommand {
 
     final static String ENDPOINT = "/v1/customer-oriented-departure";
+
+    final static ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.registerModule(new JavaTimeModule());
+        MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        MAPPER.configure(SerializationFeature.WRITE_DATES_WITH_ZONE_ID, true);
+    }
+
+    // mock though keep the same for subscribe/confirm
+    String deviceId = "D0005";
+    String messageId = "M0002";
 
     @Autowired
     DasBackendApi dasBackendApi;
@@ -36,20 +51,21 @@ class CustomerOrientedDepartureApiTest extends RestAssuredCommand {
     }
 
     @Test
-    void postSubscribe_ok() {
-        final Response response = createRequestWithHeader("en", MonitoringConstants.HEADER_REQUEST_ID)
+    void postSubscribe_ok() throws JsonProcessingException {
+        SubscribeRequest request = new SubscribeRequest();
+        request.messageId(messageId);
+        request.zugnr("37829");
+        request.deviceId(deviceId);
+        request.pushToken("t");
+        request.expiresAt(OffsetDateTime.now(ZoneId.of("UTC")).truncatedTo(ChronoUnit.MINUTES));
+        request.evu("EVU1" /* avoid traffic relevant KoA by this integration-test */);
+        request.type("REGISTER");
+        request.driver(true);
+
+        final String requestId = getRequestId();
+        final Response response = createRequestWithHeader("en", requestId)
             .contentType("application/json")
-            .body("""
-                {
-                  "messageId": "M0002",
-                  "zugnr": "37829",
-                  "deviceId": "D0005",
-                  "pushToken": "t",
-                  "expired": "2026-04-15T08:00:00Z",
-                  "evu": "EVU1",
-                  "type": "REGISTER"
-                }
-                """)
+            .body(MAPPER.writeValueAsString(request))
             .when()
             .post(getUrl(ENDPOINT + "/subscribe"))
             .then()
@@ -61,11 +77,12 @@ class CustomerOrientedDepartureApiTest extends RestAssuredCommand {
 
     @Test
     void postConfirm_ok() {
-        final Response response = createRequestWithHeader("en", MonitoringConstants.HEADER_REQUEST_ID)
-            .pathParam("messageId", "M0002")
-            .pathParam("deviceId", "D0005")
+        final String requestId = getRequestId();
+        final Response response = createRequestWithHeader("en", requestId)
+            .pathParam("messageId", messageId)
+            .pathParam("deviceId", deviceId)
             .when()
-            .post(getUrl(ENDPOINT + "/confirm/{messageId}/{deviceId}"))
+            .post(getUrl(ENDPOINT + "/confirm"))
             .then()
             .extract()
             .response();
