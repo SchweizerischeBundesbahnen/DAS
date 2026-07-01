954cd929278bb698dc049fde413166090139f849
Thomas Bomatter <thomas.bomatter@sbb.ch>
2026-07-01 09:50:31 +0200

    fix errors

--- das_backend/src/test/java/ch/sbb/das/backend/preload/domain/SferaStoreTest.java
+++ das_backend/src/test/java/ch/sbb/das/backend/preload/domain/SferaStoreTest.java
@@ -3,8 +3,6 @@ package ch.sbb.das.backend.preload.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import ch.sbb.das.backend.preload.sfera.model.v0400.SPZone;
-import ch.sbb.das.backend.preload.sfera.model.v0400.SegmentProfile;
 import ch.sbb.das.backend.preload.sfera.model.v0400.TrainCharacteristics;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,46 +16,11 @@ class SferaStoreTest {
     void setUp() {
         underTest = new SferaStore();
     }
-
-    @Test
-    void getSp_empty() {
-        SegmentProfile sp = underTest.getSp(new SegmentProfileIdentification("1", "1", "1", "1", (short) 1));
-        assertThat(sp).isNull();
-    }
-
+    
     @Test
     void getTc_empty() {
         TrainCharacteristics tc = underTest.getTc(new TrainCharacteristicsIdentification("1", "1", "1", "1"));
         assertThat(tc).isNull();
-    }
-
-    @Test
-    void addSps_ok() {
-        SegmentProfile sp = new SegmentProfile();
-        sp.setSPID("SP1");
-        sp.setSPVersionMajor("1");
-        SPZone spZone = new SPZone();
-        spZone.setIMID("0061");
-        sp.setSPZone(spZone);
-        List<SegmentProfile> sps = List.of(sp);
-
-        underTest.addSps(sps);
-
-        assertThat(underTest.getSp(new SegmentProfileIdentification("SP1", "1", null, "0061", null))).isEqualTo(sp);
-    }
-
-    @Test
-    void addDuplicateSps_throws() {
-        SegmentProfile sp = new SegmentProfile();
-        sp.setSPID("SPD");
-        sp.setSPVersionMajor("2");
-        sp.setSPVersionMinor("3");
-        SPZone spZone = new SPZone();
-        spZone.setIMID("0092");
-        sp.setSPZone(spZone);
-        List<SegmentProfile> sps = List.of(sp, sp);
-
-        assertThatThrownBy(() -> underTest.addSps(sps)).isInstanceOf(IllegalStateException.class).hasMessageContaining("Duplicate key");
     }
 
     @Test
