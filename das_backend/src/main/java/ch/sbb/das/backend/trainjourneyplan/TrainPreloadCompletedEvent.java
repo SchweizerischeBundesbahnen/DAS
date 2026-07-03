9b537673662ce2518dc5f4ad770d99610c47e318
mghilardelli <marco.ghilardelli@sbb.ch>
2026-07-03 12:45:25 +0200

    fix: save preloaded trains via service call instead of application event listener

--- das_backend/src/main/java/ch/sbb/das/backend/trainjourneyplan/TrainPreloadCompletedEvent.java
+++
@@ -1,8 +0,0 @@
-package ch.sbb.das.backend.trainjourneyplan;
-
-import java.util.Set;
-import lombok.NonNull;
-
-public record TrainPreloadCompletedEvent(@NonNull Set<Integer> trainIdentificationIds) {
-}
-
