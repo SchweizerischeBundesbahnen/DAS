baaded2c4523753eae9ef475488002ec64ca1682
mghilardelli <marco.ghilardelli@sbb.ch>
2026-07-09 11:44:18 +0200

    chore: remove h2 tests

--- das_backend/src/test/resources/createRuFeature.sql
+++
@@ -1,7 +0,0 @@
-TRUNCATE TABLE ru_feature CASCADE;
-
-ALTER SEQUENCE ru_feature_id_seq RESTART WITH 1;
-
-INSERT INTO ru_feature(id, company_code, key_value, enabled, last_modified_at, last_modified_by)
-VALUES (nextval('ru_feature_id_seq'), '1111', 'CHECKLIST_DEPARTURE_PROCESS', TRUE, '2025-04-17 10:18:34',
-        'unit_test');
