fc6d0d87a711c8d6ad288ab4d7cf86d7eceb8e20
mghilardelli <marco.ghilardelli@sbb.ch>
2026-07-09 13:12:10 +0200

    chore: internal company description

--- das_backend/src/main/java/ch/sbb/das/backend/companies/internal/InternalCompany.java
+++ das_backend/src/main/java/ch/sbb/das/backend/companies/internal/InternalCompany.java
@@ -4,9 +4,9 @@ import ch.sbb.das.backend.companies.CompanyCode;
 import ch.sbb.das.backend.companies.CompanyShortName;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "A company with internal id for CRUD operations (Admin only)")
+@Schema(description = "A company with technical id for CRUD operations (Admin only)")
 record InternalCompany(
-    @Schema(description = "The internal identifier.", requiredMode = Schema.RequiredMode.REQUIRED) Integer id,
+    @Schema(description = "The technical identifier.", requiredMode = Schema.RequiredMode.REQUIRED) Integer id,
     @Schema(description = "The RICS company code.", requiredMode = Schema.RequiredMode.REQUIRED) CompanyCode code,
     @Schema(description = "The human-readable short name.", requiredMode = Schema.RequiredMode.REQUIRED) CompanyShortName shortName) {
 
