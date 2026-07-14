6d301dae52d4ca9ff39b1035bab9706402e2c704
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-07-14 13:44:33 +0200

    chore: formatting according to SBBStyle

--- das_backend/src/main/java/ch/sbb/das/backend/features/internal/InternalRuFeatureResponse.java
+++ das_backend/src/main/java/ch/sbb/das/backend/features/internal/InternalRuFeatureResponse.java
@@ -3,6 +3,7 @@ package ch.sbb.das.backend.features.internal;
 import ch.sbb.das.backend.common.ApiResponse;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record InternalRuFeatureResponse(
