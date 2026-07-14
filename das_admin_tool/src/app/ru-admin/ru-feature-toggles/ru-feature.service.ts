1442162e67205f0cce3749f530b191d8e360d6eb
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-07-14 10:01:48 +0200

    feat: add ru feature service

---
+++ das_admin_tool/src/app/ru-admin/ru-feature-toggles/ru-feature.service.ts
@@ -0,0 +1,47 @@
+import {inject, Injectable} from '@angular/core';
+import {firstValueFrom} from 'rxjs';
+import {RuAdminApi, RuFeature} from '../ru-admin-api';
+import {
+  RuFeatureDialogEditResult,
+  RuFeatureToggleDialog
+} from './ru-feature-toggle-dialog/ru-feature-toggle-dialog.component';
+import {BaseDialogService} from '../base-dialog.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RuFeatureService extends BaseDialogService {
+  private readonly ruAdminApi = inject(RuAdminApi);
+  readonly ruFeaturesResource = this.ruAdminApi.ruFeatures;
+
+  async edit(ruFeature: RuFeature): Promise<void> {
+    const event = await firstValueFrom(this.dialogService.open<RuFeatureToggleDialog, RuFeatureDialogEditResult>(RuFeatureToggleDialog, {data: ruFeature}).afterClosed);
+    if (event.result === 'delete') {
+      await this.runMutation(
+        this.ruAdminApi.deleteRuFeature(ruFeature.id!),
+        $localize`:@@toast_delete_success:Der Eintrag wurde erfolgreich gelöscht.`,
+      );
+    } else if (event.result && ruFeature.id) {
+      await this.runMutation(
+        this.ruAdminApi.putRuFeature(ruFeature.id, event.result),
+        $localize`:@@ru_feature_toggles_toast_edit_success:Das Feature wurde erfolgreich gespeichert.`,
+        [event.result.companyCode],
+      );
+    }
+  }
+
+  async add(): Promise<void> {
+    const event = await firstValueFrom(this.dialogService.open<RuFeatureToggleDialog, RuFeature>(RuFeatureToggleDialog).afterClosed);
+    if (event.result) {
+      await this.runMutation(
+        this.ruAdminApi.postRuFeature(event.result),
+        $localize`:@@ru_feature_toggles_toast_create_success:Das Feature wurde erfolgreich erstellt.`,
+        [event.result.companyCode],
+      );
+    }
+  }
+
+  protected override reload(): void {
+    this.ruAdminApi.ruFeatures.reload();
+  }
+}
