3574fc60f101a04f844c5ce40740eb564847f8ec
mghilardelli <marco.ghilardelli@sbb.ch>
2026-07-22 17:42:32 +0200

    chore: extract mappers for formation/cargo mapping

--- das_backend/src/main/java/ch/sbb/das/backend/cargo/api/v1/model/FormationRun.java
+++ das_backend/src/main/java/ch/sbb/das/backend/cargo/api/v1/model/FormationRun.java
@@ -1,6 +1,5 @@
 package ch.sbb.das.backend.cargo.api.v1.model;
 
-import ch.sbb.das.backend.cargo.infrastructure.model.TrainFormationRunEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
@@ -95,55 +94,4 @@ public record FormationRun(
     @Schema(requiredMode = RequiredMode.NOT_REQUIRED)
     String slopeMaxForHoldingForceMinInPermille
 ) {
-
-    private static FormationRun from(TrainFormationRunEntity trainFormationRunEntity) {
-        //      todo  consider use mapper (cause of testing effort)
-        return FormationRun.builder()
-            .inspectionDateTime(trainFormationRunEntity.getInspectionDateTime())
-            .tafTapLocationReferenceStart(trainFormationRunEntity.getTafTapLocationReferenceStart())
-            .tafTapLocationReferenceEnd(trainFormationRunEntity.getTafTapLocationReferenceEnd())
-            .trainCategoryCode(trainFormationRunEntity.getTrainCategoryCode())
-            .brakedWeightPercentage(trainFormationRunEntity.getBrakedWeightPercentage())
-            .tractionMaxSpeedInKmh(trainFormationRunEntity.getTractionMaxSpeedInKmh())
-            .hauledLoadMaxSpeedInKmh(trainFormationRunEntity.getHauledLoadMaxSpeedInKmh())
-            .formationMaxSpeedInKmh(trainFormationRunEntity.getFormationMaxSpeedInKmh())
-            .tractionLengthInCm(trainFormationRunEntity.getTractionLengthInCm())
-            .hauledLoadLengthInCm(trainFormationRunEntity.getHauledLoadLengthInCm())
-            .formationLengthInCm(trainFormationRunEntity.getFormationLengthInCm())
-            .tractionWeightInT(trainFormationRunEntity.getTractionWeightInT())
-            .hauledLoadWeightInT(trainFormationRunEntity.getHauledLoadWeightInT())
-            .formationWeightInT(trainFormationRunEntity.getFormationWeightInT())
-            .tractionBrakedWeightInT(trainFormationRunEntity.getTractionBrakedWeightInT())
-            .hauledLoadBrakedWeightInT(trainFormationRunEntity.getHauledLoadBrakedWeightInT())
-            .formationBrakedWeightInT(trainFormationRunEntity.getFormationBrakedWeightInT())
-            .tractionHoldingForceInHectoNewton(trainFormationRunEntity.getTractionHoldingForceInHectoNewton())
-            .hauledLoadHoldingForceInHectoNewton(trainFormationRunEntity.getHauledLoadHoldingForceInHectoNewton())
-            .formationHoldingForceInHectoNewton(trainFormationRunEntity.getFormationHoldingForceInHectoNewton())
-            .brakePositionGForLeadingTraction(trainFormationRunEntity.getBrakePositionGForLeadingTraction())
-            .brakePositionGForBrakeUnit1to5(trainFormationRunEntity.getBrakePositionGForBrakeUnit1to5())
-            .brakePositionGForLoadHauled(trainFormationRunEntity.getBrakePositionGForLoadHauled())
-            .simTrain(trainFormationRunEntity.getSimTrain())
-            .additionalTractions(trainFormationRunEntity.getAdditionalTractions())
-            .carCarrierVehicle(trainFormationRunEntity.getCarCarrierVehicle())
-            .dangerousGoods(trainFormationRunEntity.getDangerousGoods())
-            .vehiclesCount(trainFormationRunEntity.getVehiclesCount())
-            .vehiclesWithBrakeDesignLlAndKCount(trainFormationRunEntity.getVehiclesWithBrakeDesignLAndLlAndKCount()) // todo: delete as soon as das_client v 0.49.0 released
-            .vehiclesWithBrakeDesignLAndLlAndKCount(trainFormationRunEntity.getVehiclesWithBrakeDesignLAndLlAndKCount())
-            .vehiclesWithBrakeDesignDCount(trainFormationRunEntity.getVehiclesWithBrakeDesignDCount())
-            .vehiclesWithDisabledBrakesCount(trainFormationRunEntity.getVehiclesWithDisabledBrakesCount())
-            .europeanVehicleNumberFirst(trainFormationRunEntity.getEuropeanVehicleNumberFirst())
-            .europeanVehicleNumberLast(trainFormationRunEntity.getEuropeanVehicleNumberLast())
-            .axleLoadMaxInKg(trainFormationRunEntity.getAxleLoadMaxInKg())
-            .routeClass(trainFormationRunEntity.getRouteClass())
-            .gradientUphillMaxInPermille(trainFormationRunEntity.getGradientUphillMaxInPermille())
-            .gradientDownhillMaxInPermille(trainFormationRunEntity.getGradientDownhillMaxInPermille())
-            .slopeMaxForHoldingForceMinInPermille(trainFormationRunEntity.getSlopeMaxForHoldingForceMinInPermille())
-            .build();
-    }
-
-    static List<FormationRun> fromList(List<TrainFormationRunEntity> trainFormationRunEntities) {
-        return trainFormationRunEntities.stream()
-            .map(FormationRun::from)
-            .toList();
-    }
 }
