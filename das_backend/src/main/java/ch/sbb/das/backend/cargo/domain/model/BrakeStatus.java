4596c10d209062d4d65187258d09b53a2af0de69
mghilardelli <marco.ghilardelli@sbb.ch>
2026-07-22 18:35:52 +0200

    feat: log brakestatus null

--- das_backend/src/main/java/ch/sbb/das/backend/cargo/domain/model/BrakeStatus.java
+++ das_backend/src/main/java/ch/sbb/das/backend/cargo/domain/model/BrakeStatus.java
@@ -3,10 +3,12 @@ package ch.sbb.das.backend.cargo.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Slf4j
 public class BrakeStatus {
 
     private static final int DISABLED_BRAKE_STATUS = 0;
@@ -14,6 +16,7 @@ public class BrakeStatus {
 
     boolean isDisabled() {
         if (brakeStatus == null) {
+            log.error("Should not happen: BrakeStatus is null");
             return false;
         }
         return brakeStatus == DISABLED_BRAKE_STATUS;
