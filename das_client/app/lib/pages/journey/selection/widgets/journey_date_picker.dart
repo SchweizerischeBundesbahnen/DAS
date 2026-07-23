45f7f28c50cd69409453bef7f3d5e0b70e8bcf56
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-07-23 07:33:25 +0200

    chore: use v5 DSM picker for date selection

--- das_client/app/lib/pages/journey/selection/widgets/journey_date_picker.dart
+++ das_client/app/lib/pages/journey/selection/widgets/journey_date_picker.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:app/i18n/i18n.dart';
-import 'package:app/theme/theme_util.dart';
 import 'package:app/util/format.dart';
 import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
@@ -23,42 +24,15 @@ class JourneyDatePicker extends StatelessWidget {
   Widget build(BuildContext context) => SBBContentBox(
     key: datePickerKey,
     margin: .symmetric(vertical: SBBSpacing.xSmall),
-    padding: .symmetric(vertical: SBBSpacing.xSmall, horizontal: SBBSpacing.medium),
-    child: Column(
-      spacing: SBBSpacing.xSmall,
-      mainAxisSize: .min,
-      children: availableStartDates.map((d) => _dateItem(context, d)).toList(),
+    padding: .symmetric(vertical: SBBSpacing.xSmall),
+    child: SBBPicker.list(
+      initialSelectedIndex: availableStartDates.indexOf(selectedDate),
+      onSelectedItemChanged: (idx) => onChanged.call(availableStartDates[idx]),
+      items: availableStartDates.map((d) => _formattedDate(d, context)).toList(growable: false),
+      looping: false,
+      visibleItemCount: max(availableStartDates.length, 3),
     ),
   );
-
-  Widget _dateItem(BuildContext context, DateTime d) {
-    final isSelected = d == selectedDate;
-    final unselectedTextStyle = SBBTextStyles.xLargeLight.romanStyle.copyWith(
-      color: ThemeUtil.getColor(context, SBBColors.storm, SBBColors.graphite),
-    );
-    final selectedTextStyle = SBBTextStyles.xLargeLight.romanStyle.copyWith(
-      color: ThemeUtil.getColor(context, SBBColors.black, SBBColors.white),
-    );
-    return GestureDetector(
-      onTap: () => onChanged.call(d),
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 200),
-        curve: Curves.easeInOut,
-        constraints: BoxConstraints(minWidth: double.infinity),
-        padding: .symmetric(vertical: 2.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(SBBSpacing.xSmall)),
-          color: isSelected ? ThemeUtil.getColor(context, SBBColors.cloud, SBBColors.iron) : SBBColors.transparent,
-        ),
-        child: Center(
-          child: Text(
-            _formattedDate(d, context),
-            style: isSelected ? selectedTextStyle : unselectedTextStyle,
-          ),
-        ),
-      ),
-    );
-  }
 
   String _formattedDate(DateTime date, BuildContext context) {
     if (DateUtils.isSameDay(date, clock.now())) return context.l10n.c_today;
