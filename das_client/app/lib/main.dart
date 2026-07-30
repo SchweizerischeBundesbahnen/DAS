36791076ba397d536917ac7c0c3d8613352aafeb
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-07-30 08:48:45 +0200

    feat: tms vad is default true in app start

--- das_client/app/lib/main.dart
+++ das_client/app/lib/main.dart
@@ -53,10 +53,10 @@ Future<void> _initDependencyInjection(Flavor flavor, DASLogger? dasLogger) async
   if (dasLogger != null) {
     GetIt.I.registerLogger(logger: dasLogger);
   }
-  // TODO: The problem here is that someone who still has a session with TMS authenticator
-  // will not seem to be logged in anymore since we assume SferaMock as the default in app start.
+  // TODO: The problem here is that someone who still has a session with Sfera mock authenticator
+  // will not seem to be logged in anymore since we assume Tms as the default in app start.
   // This is necessary to ensure that an authenticator is available for the SplashPage
-  await scopeHandler.push<SferaMockScope>();
+  await scopeHandler.push<TmsScope>();
 }
 
 Future<void> _initMessaging(Flavor flavor) async {
