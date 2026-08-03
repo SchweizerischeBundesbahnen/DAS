1b5a7c9b86df976201910ae9c6f1d6bd9d68f9d5
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-08-03 07:54:16 +0200

    test: fix android by checking on closed stream

--- das_client/app/lib/pages/journey/journey_screen/detail_modal/detail_modal_view_model.dart
+++ das_client/app/lib/pages/journey/journey_screen/detail_modal/detail_modal_view_model.dart
@@ -38,7 +38,9 @@ class DetailModalViewModel {
 
   void _initController() {
     controller = DASModalSheetController(
-      onClose: () => _rxOpenModalType.add(null),
+      onClose: () {
+        if (!_rxOpenModalType.isClosed) _rxOpenModalType.add(null);
+      },
     );
   }
 
@@ -83,7 +85,7 @@ class DetailModalViewModel {
 
   void close() {
     controller.close();
-    _rxContentBuilder.add(null);
+    if (!_rxContentBuilder.isClosed) _rxContentBuilder.add(null);
     _openContentKey = null;
   }
 
