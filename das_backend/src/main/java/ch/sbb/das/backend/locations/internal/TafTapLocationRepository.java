1dc6c8d356746f3b931826edd98e52119f626146
Marco <marco.ghilardelli@sbb.ch>
2026-08-03 17:24:13 +0200

    fix: db credentials rotation (#2541)

--- das_backend/src/main/java/ch/sbb/das/backend/locations/internal/TafTapLocationRepository.java
+++ das_backend/src/main/java/ch/sbb/das/backend/locations/internal/TafTapLocationRepository.java
@@ -1,9 +1,9 @@
 package ch.sbb.das.backend.locations.internal;
 
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TafTapLocationRepository extends ListCrudRepository<TafTapLocationEntity, Integer> {
+public interface TafTapLocationRepository extends JpaRepository<TafTapLocationEntity, Integer> {
 
 }
--- das_backend/src/main/java/ch/sbb/das/backend/locations/internal/TafTapLocationsImportService.java
+++ das_backend/src/main/java/ch/sbb/das/backend/locations/internal/TafTapLocationsImportService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -24,11 +25,13 @@ public class TafTapLocationsImportService {
 
     @Scheduled(cron = "${atlas.import-cron}")
     @SchedulerLock(name = "importLocations", lockAtLeastFor = "${shedlock.lock-at-least-for:10m}")
+    @Transactional
     public void importLocations() {
         log.info("Starting scheduled location import");
         List<TafTapLocationEntity> locations = getLocations();
-        tafTapLocationRepository.deleteAll();
+        tafTapLocationRepository.deleteAllInBatch();
         tafTapLocationRepository.saveAll(locations);
+        tafTapLocationRepository.flush();
         log.info("Finished location import with {} locations", locations.size());
     }
 
