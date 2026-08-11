35e980874bf3773e1d5550165e52e9627cd00d7a
DevCore <62793354+rawi-coding@users.noreply.github.com>
2026-08-11 17:27:55 +0200

    fix: close modal after brightness permission is given (#2469) (#2611)

--- das_client/app/lib/brightness/brightness_modal_sheet.dart
+++ das_client/app/lib/brightness/brightness_modal_sheet.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:app/brightness/brightness_manager.dart';
 import 'package:app/di/di.dart';
 import 'package:app/i18n/i18n.dart';
+import 'package:app/util/app_lifecycle_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
-class BrightnessModalSheet extends StatelessWidget {
+class BrightnessModalSheet extends StatefulWidget {
   const BrightnessModalSheet({super.key});
 
   static Future<void> openIfNeeded(BuildContext context) async {
@@ -17,6 +20,34 @@ class BrightnessModalSheet extends StatelessWidget {
         titleText: context.l10n.w_modal_sheet_permissions_title,
         body: const BrightnessModalSheet(),
       );
+    }
+  }
+
+  @override
+  State<BrightnessModalSheet> createState() => _BrightnessModalSheetState();
+}
+
+class _BrightnessModalSheetState extends State<BrightnessModalSheet> {
+  StreamSubscription<void>? _onResumedSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _onResumedSubscription = DI.get<AppLifecycleViewModel>().onResumed.listen((_) => _closeIfPermissionGranted());
+  }
+
+  @override
+  void dispose() {
+    _onResumedSubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _closeIfPermissionGranted() async {
+    final brightnessManager = DI.get<BrightnessManager>();
+    final hasPermission = await brightnessManager.hasWriteSettingsPermission();
+
+    if (hasPermission && mounted) {
+      Navigator.of(context).pop();
     }
   }
 
