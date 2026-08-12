4a51dbc0c5db8fe83d953032b2cbc4c5213245f7
Thomas Bomatter <thomas.bomatter@sbb.ch>
2026-08-12 13:27:29 +0200

    cleanup

--- das_client/sfera/lib/src/data/dto/fixed_point_relevance_nsp_dto.dart
+++ das_client/sfera/lib/src/data/dto/fixed_point_relevance_nsp_dto.dart
@@ -1,8 +1,5 @@
 import 'package:sfera/src/data/dto/network_specific_parameter_dto.dart';
 
-/// Network Specific Parameter attribute name is 'newSpeed'
-/// In domain, often referred to as 'VProSpeed'
-/// In application layer, referred to as calculatedSpeed, since this speed is calculated shortly before the journey.
 class FixedPointRelevanceNspDto extends NetworkSpecificParameterDto {
   static const String elementName = 'fixedPointRelevance';
 
--- das_client/sfera/test/src/data/mapper/sfera_model_mapper_test.dart
+++ das_client/sfera/test/src/data/mapper/sfera_model_mapper_test.dart
@@ -295,7 +295,7 @@ void main() {
     expect(journey.metadata.nonStandardTrackEquipmentSegments[0].endOrder, 5000);
   });
 
-  test('Test servicePoints are parsed correctly', () async {
+  test('Test signals are parsed correctly', () async {
     final journey = getJourney('T9999');
     final signals = journey.data.where((it) => it.dataType == .signal).cast<Signal>().toList();
 
@@ -2127,7 +2127,7 @@ void main() {
     expect(speedRestrictions[0].kmTo, 56.062);
   });
 
-  test('Test nsp servicePoints', () async {
+  test('Test nsp signals', () async {
     final journey = getJourney('T45');
     expect(journey.valid, true);
 
