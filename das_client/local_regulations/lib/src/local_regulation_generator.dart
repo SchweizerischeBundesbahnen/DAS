8e28423895f3159d7472e4bb7f57d95c91d4e00d
u221638 <ralf.winkelmann2@sbb.ch>
2026-08-27 13:53:20 +0200

    chore: migrate local_regulation component to primary constructors (#2721)

--- das_client/local_regulations/lib/src/local_regulation_generator.dart
+++ das_client/local_regulations/lib/src/local_regulation_generator.dart
@@ -2,8 +2,6 @@ import 'dart:core';
 
 import 'package:sfera/component.dart';
 
-abstract class LocalRegulationHtmlGenerator {
-  LocalRegulationHtmlGenerator._();
-
+abstract class LocalRegulationHtmlGenerator._() {
   String generate({required List<LocalRegulationSection> sections});
 }
--- das_client/local_regulations/lib/src/local_regulation_relevance.dart
+++ das_client/local_regulations/lib/src/local_regulation_relevance.dart
@@ -1,7 +1,7 @@
 import 'package:collection/collection.dart';
 
 /// Describes for what/who the local regulation is relevant
-enum LocalRegulationRelevance {
+enum LocalRegulationRelevance({required final String abbreviation}) {
   /// Local regulations for train journeys and shunting manoeuvres
   trainJourneysAndShuntingManoeuvres(abbreviation: 'ZR'),
 
@@ -16,10 +16,6 @@ enum LocalRegulationRelevance {
 
   /// Local regulations for traffic controllers
   trafficControllers(abbreviation: 'Fdl');
-
-  LocalRegulationRelevance({required this.abbreviation});
-
-  final String abbreviation;
 
   /// Extracts the relevance from a string that starts with the abbreviation.
   /// Example: "GV Ortsfestes französisches Signal"
