a8ab8b89fcf149319554a85b3d9a90aae739b476
u221638 <ralf.winkelmann2@sbb.ch>
2026-08-31 13:24:55 +0200

    fix: wrong constructor migration

--- das_client/sfera/lib/src/data/dto/amount_tram_signals_dto.dart
+++ das_client/sfera/lib/src/data/dto/amount_tram_signals_dto.dart
@@ -1,6 +1,6 @@
 import 'package:sfera/src/data/dto/network_specific_parameter_dto.dart';
 
-class AmountTramSignalsDto({super.type = elementName, super.attributes, super.children, super.value})
+class AmountTramSignalsDto({super.type, super.attributes, super.children, super.value})
     extends NetworkSpecificParameterDto {
   static const String elementName = 'amountTramSignals';
 
--- das_client/sfera/lib/src/data/dto/curve_point_network_specific_point_dto.dart
+++ das_client/sfera/lib/src/data/dto/curve_point_network_specific_point_dto.dart
@@ -2,7 +2,7 @@ import 'package:sfera/src/data/dto/network_specific_parameter_dto.dart';
 import 'package:sfera/src/data/dto/network_specific_point_dto.dart';
 import 'package:sfera/src/data/dto/xml_curve_speed_dto.dart';
 
-class CurvePointNetworkSpecificPointDto({super.type = groupNameValue, super.attributes, super.children, super.value})
+class CurvePointNetworkSpecificPointDto({super.type, super.attributes, super.children, super.value})
     extends NetworkSpecificPointDto {
   static const String groupNameValue = 'curvePoint';
 
