d495620243fa818361f8ce9b31390ff73cd2fa1b
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-09-09 09:49:13 +0200

    feat: extend line speed view model

--- das_client/app/lib/pages/journey/journey_screen/view_model/line_speed_view_model.dart
+++ das_client/app/lib/pages/journey/journey_screen/view_model/line_speed_view_model.dart
@@ -7,19 +7,18 @@ class LineSpeedViewModel({
   required final JourneySettingsViewModel _journeySettingsViewModel,
   super.journeyViewModel,
 }) extends JourneyAwareViewModel {
-  ResolvedTrainSeriesSpeed getResolvedSpeedForOrder(int order) {
+  ResolvedTrainSeriesSpeed getResolvedSpeedForOrder(int order, {BrakeSeries? brakeSeries}) {
     final metadata = lastJourney?.metadata;
     if (metadata == null) return ResolvedTrainSeriesSpeed.none();
 
-    final settings = _journeySettingsViewModel.modelValue;
-    final brakeSeries = settings.currentBrakeSeries;
+    final resolvedBrakeSeries = brakeSeries ?? _journeySettingsViewModel.modelValue.currentBrakeSeries;
 
     var trainSeriesSpeeds = metadata.lineSpeeds[order];
     var isPrevious = false;
 
-    if (!_hasSpeed(trainSeriesSpeeds, brakeSeries)) {
+    if (!_hasSpeed(trainSeriesSpeeds, resolvedBrakeSeries)) {
       var lastKey = metadata.lineSpeeds.lastKeyBefore(order);
-      while (!_hasSpeed(trainSeriesSpeeds, brakeSeries) && lastKey != null) {
+      while (!_hasSpeed(trainSeriesSpeeds, resolvedBrakeSeries) && lastKey != null) {
         trainSeriesSpeeds = metadata.lineSpeeds[lastKey];
         lastKey = metadata.lineSpeeds.lastKeyBefore(lastKey);
         isPrevious = true;
@@ -27,8 +26,8 @@ class LineSpeedViewModel({
     }
 
     final speed = trainSeriesSpeeds?.speedFor(
-      brakeSeries?.trainSeries,
-      brakedWeightPercentage: brakeSeries?.brakedWeightPercentage,
+      resolvedBrakeSeries?.trainSeries,
+      brakedWeightPercentage: resolvedBrakeSeries?.brakedWeightPercentage,
     );
 
     return ResolvedTrainSeriesSpeed(
