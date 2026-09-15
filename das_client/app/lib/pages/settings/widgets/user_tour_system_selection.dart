316b5217daf428d93dc4ad13556c25b564e4e339
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-09-15 14:41:34 +0200

    feat: refactor user tour system selection

--- das_client/app/lib/pages/settings/widgets/user_tour_system_selection.dart
+++ das_client/app/lib/pages/settings/widgets/user_tour_system_selection.dart
@@ -1,50 +1,47 @@
-import 'package:app/di/di.dart';
 import 'package:app/i18n/i18n.dart';
 import 'package:app/model/tour_system.dart';
-import 'package:app/provider/local_key_value_store.dart';
+import 'package:app/pages/settings/view_model/model/user_settings_model.dart';
+import 'package:app/pages/settings/view_model/user_settings_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 
-class UserTourSystemSelection extends StatefulWidget {
-  const UserTourSystemSelection({super.key});
-
-  @override
-  State<UserTourSystemSelection> createState() => _UserTourSystemSelectionState();
-}
-
-class _UserTourSystemSelectionState extends State<UserTourSystemSelection> {
-  final _userSettings = DI.get<LocalKeyValueStore>();
+class TourSystemSetting extends StatelessWidget {
+  const TourSystemSetting({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: SBBSpacing.xSmall),
-      child: Column(
-        spacing: SBBSpacing.xSmall,
-        crossAxisAlignment: .start,
-        children: [
-          SBBListHeader(
-            context.l10n.w_user_tour_system_selection_label,
-            style: SBBListHeaderStyle(padding: .symmetric(horizontal: SBBSpacing.medium)),
-          ),
-          SBBContentBox(
-            child: SBBDropdown<TourSystem?>(
-              triggerDecoration: SBBInputDecoration(
-                placeholderText: context.l10n.w_user_tour_system_selection_label,
-              ),
-              sheetConfig: SBBBottomSheetConfig(
-                titleText: context.l10n.w_user_tour_system_selection_title,
-              ),
-              selectedItem: _userSettings.tourSystem,
-              items: _tourSystemItems(context),
-              onChanged: (selected) {
-                _userSettings.set(.tourSystem, selected?.name);
-                setState(() {});
-              },
+    final viewModel = context.read<UserSettingsViewModel>();
+    return StreamBuilder<UserSettingsModel>(
+      stream: viewModel.model,
+      initialData: viewModel.modelValue,
+      builder: (context, snapshot) {
+        final settings = snapshot.data ?? const UserSettingsModel();
+        return Column(
+          mainAxisSize: .min,
+          spacing: SBBSpacing.xxSmall,
+          crossAxisAlignment: .start,
+          children: [
+            SBBListHeader(
+              context.l10n.w_user_tour_system_selection_label,
+              style: SBBListHeaderStyle(padding: .only(left: SBBSpacing.medium)),
             ),
-          ),
-        ],
-      ),
+            SBBContentBox(
+              child: SBBDropdown<TourSystem?>(
+                triggerDecoration: SBBInputDecoration(
+                  placeholderText: context.l10n.w_user_tour_system_selection_label,
+                ),
+                sheetConfig: SBBBottomSheetConfig(
+                  titleText: context.l10n.w_user_tour_system_selection_title,
+                ),
+                selectedItem: settings.tourSystem,
+                items: _tourSystemItems(context),
+                onChanged: viewModel.updateTourSystem,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
